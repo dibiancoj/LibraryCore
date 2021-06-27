6ffe8d1f1d2dc2610eed85ee349306578ed87b3d
Jason <dibiancoj@gmail.com>
2021-06-27 14:02:09 -0400

    date time utilities

---
+++ Src/LibraryCore.Core/DateTimeUtilities/DateTimeUtility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LibraryCore.Core.DateTimeUtilities
+{
+    public static class DateTimeUtility
+    {
+        /// <summary>
+        /// Get the age of a person, date, etc.
+        /// </summary>
+        /// <param name="dateOfBirth">birth date</param>
+        /// <returns>Age in years</returns>
+        public static int Age(DateTime dateOfBirth)
+        {
+            //grab the date today
+            var today = DateTime.Today;
+
+            //calculate the age
+            var age = today.Year - dateOfBirth.Year;
+
+            //go back to the year if the person was born on a leap year
+            if (dateOfBirth > today.AddYears(-age))
+            {
+                age--;
+            }
+
+            //return the age
+            return age;
+        }
+
+    }
+}
---
+++ Tests/LibraryCore.Tests.Core/DateTimeUtilities/DateTimeUtilityTest.cs
@@ -0,0 +1,33 @@
+﻿using LibraryCore.Core.DateTimeUtilities;
+using System;
+using Xunit;
+
+namespace LibraryCore.Tests.Core.DateTimeUtilities
+{
+    public class DateTimeUtilityTest
+    {
+        [Fact]
+        public void CalculateAgeTest1()
+        {
+            Assert.Equal(0, DateTimeUtility.Age(DateTime.Today.AddDays(-5)));
+        }
+
+        [Fact]
+        public void CalculateAgeTest2()
+        {
+            Assert.Equal(1, DateTimeUtility.Age(DateTime.Today.AddYears(-1).AddDays(-1)));
+        }
+
+        [Fact]
+        public void CalculateAgeTest3()
+        {
+            Assert.Equal(2, DateTimeUtility.Age(DateTime.Today.AddYears(-2)));
+        }
+
+        [Fact]
+        public void CalculateAgeTest4()
+        {
+            Assert.Equal(2, DateTimeUtility.Age(DateTime.Today.AddYears(-2).AddDays(-3)));
+        }
+    }
+}
