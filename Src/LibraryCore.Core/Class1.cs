e734268138c40f3713b30f1c7f09a7d13d173c04
Jason <dibiancoj@gmail.com>
2021-06-27 12:51:17 -0400

    seed the project

---
+++ Src/LibraryCore.Core/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace LibraryCore.Core
+{
+    public class Class1
+    {
+        public static string Seed() => "Hello World";
+    }
+}
---
+++ Tests/LibraryCore.Tests.Core/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LibraryCore.Tests.Core
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal("Hello World", LibraryCore.Core.Class1.Seed());
+        }
+    }
+}
