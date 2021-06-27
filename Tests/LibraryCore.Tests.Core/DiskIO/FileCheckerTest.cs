3c799c0586f3f5059b649017f88d29f51599e6dd
Jason <dibiancoj@gmail.com>
2021-06-27 14:09:40 -0400

    add file checker

---
+++ Tests/LibraryCore.Tests.Core/DiskIO/FileCheckerTest.cs
@@ -0,0 +1,22 @@
+﻿using LibraryCore.Core.DiskIO;
+using System.Text;
+using Xunit;
+
+namespace LibraryCore.Tests.Core.DiskIO
+{
+    public class FileCheckerTest
+    {
+        [InlineData("MZ", true)]
+        [InlineData("M", false)] //only 1 character
+        [InlineData("123", false)]
+        [InlineData("ab", false)]
+        [InlineData("c", false)]
+        [InlineData("ef", false)]
+        [InlineData("sdfasdfasd", true)]
+        [Theory]
+        public void IsExecutablePositiveTest1(string FirstTwoBytesToTest, bool expectResultIsExe)
+        {
+            Assert.Equal(expectResultIsExe, FileChecker.IsExecutableInWindows(Encoding.ASCII.GetBytes(FirstTwoBytesToTest)));
+        }
+    }
+}
