520e95a2806e0dd8420dcfb3af431d23dd551528
Jason <dibiancoj@gmail.com>
2021-06-27 14:20:33 -0400

    commit

--- Src/LibraryCore.Core/DiskIO/FileChecker.cs
+++ Src/LibraryCore.Core/DiskIO/FileChecker.cs
@@ -9,12 +9,11 @@ namespace LibraryCore.Core.DiskIO
         /// Determines if a file is an executable by looking at the first 2 bytes. Handles scenario's where the user modifies the file extension. 
         /// </summary>
         /// <param name="fileToInspect">byte array which contains the file you want to inspect</param>
-        /// <returns>Is it an executable</returns>]
+        /// <returns>Is it an executable</returns>
         public static bool IsExecutableInWindows(ReadOnlySpan<byte> fileToInspect)
         {
             //now convert that to a string and compare it against MZ (which is an executable in windows)
             return fileToInspect.Length >= 2 && Encoding.UTF8.GetString(fileToInspect.Slice(0, 2)) == "MZ";
         }
-
     }
 }
