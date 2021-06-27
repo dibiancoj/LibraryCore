8f239031dc486105e00737b11f877df01ee36ab7
Jason <dibiancoj@gmail.com>
2021-06-27 13:54:37 -0400

    mutable data type structure.

---
+++ Tests/LibraryCore.Tests.Core/DataTypes/MutableKeyValuePairTest.cs
@@ -0,0 +1,30 @@
+﻿using LibraryCore.Core.DataTypes;
+using Xunit;
+
+namespace LibraryCore.Tests.Core.DataTypes
+{
+    public class MutableKeyValuePairTest
+    {
+        [Fact]
+        public void PropertiesWithoutConstructorSettingAreCorrect()
+        {
+            var model = new MutableKeyValuePair<string, string>
+            {
+                Key = "key1",
+                Value = "value1"
+            };
+
+            Assert.Equal("key1", model.Key);
+            Assert.Equal("value1", model.Value);
+        }
+
+        [Fact]
+        public void PropertiesAreCorrect()
+        {
+            var model = new MutableKeyValuePair<string, string>("key1", "value1");
+
+            Assert.Equal("key1", model.Key);
+            Assert.Equal("value1", model.Value);
+        }
+    }
+}
