d389ba210ddc9fb56fb9b0bdd95b0cfa06b46b06
Jason <dibiancoj@gmail.com>
2021-06-29 16:35:49 -0400

    regular expressions

---
+++ Src/LibraryCore.Core/Reflection/ReflectionUtilties.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+
+namespace LibraryCore.Core.Reflection
+{
+    [ExcludeFromCodeCoverage]
+    public static class ReflectionUtilties
+    {
+
+        /// <summary>
+        /// Scans assemblies to find all instances of TInterface ie: register all classes that implement IRepository
+        /// </summary>
+        /// <typeparam name="TInterface">Interface type for the types that you want to register</typeparam>
+        /// <returns>All the types that implement that interface</returns>
+        public static IEnumerable<TypeInfo> ScanForAllInstancesOfType<TInterface>()
+        {
+            //NOT TESTABLE - No App Domain In Unit Test Project
+            return (Assembly.GetEntryAssembly() ?? throw new Exception("No Entry Assembly"))
+                   .GetReferencedAssemblies()
+                   .Select(Assembly.Load)
+                   .SelectMany(x => x.DefinedTypes)
+                   .Where(x => x.ImplementedInterfaces.Contains(typeof(TInterface)));
+        }
+
+    }
+}
