f9c4cf87b091a18666748967aabcad2943e4d2d8
Jason <dibiancoj@gmail.com>
2021-06-29 17:25:44 -0400

    uri util

---
+++ Src/LibraryCore.Core/UrlUtilities/UrlUtility.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LibraryCore.Core.UrlUtilities
+{
+    public static class UrlUtility
+    {
+        /// <summary>
+        /// If the uri to transform is a relative uri then it will transform the uri into an absolute uri. If its already absolute then it will return the uri passed in
+        /// </summary>
+        /// <param name="urlToTransform">Uri to make absolute</param>
+        /// <param name="baseUrlWebSite">Uri of the web site that is the base address.</param>
+        /// <returns>Absolute Uri</returns>
+        public static Uri MakeRelativeUriAbsolute(string urlToTransform, Uri baseUrlWebSite)
+        {
+            return MakeRelativeUriAbsolute(new Uri(urlToTransform, UriKind.RelativeOrAbsolute), baseUrlWebSite);
+        }
+
+        /// <summary>
+        /// If the uri to transform is a relative uri then it will transform the uri into an absolute uri. If its already absolute then it will return the uri passed in
+        /// </summary>
+        /// <param name="urlToTransform">Uri to make absolute</param>
+        /// <param name="baseUrlWebSite">Uri of the web site that is the base address.</param>
+        /// <returns>Absolute Uri</returns>
+        public static Uri MakeRelativeUriAbsolute(Uri urlToTransform, Uri baseUrlWebSite)
+        {
+            //uri's don't handle www. as an absolute value. We want to treat that as absolute since we don't know what that value is.
+            return urlToTransform.IsAbsoluteUri || urlToTransform.ToString().StartsWith("www", StringComparison.OrdinalIgnoreCase) ?
+                urlToTransform :
+                new Uri(baseUrlWebSite, urlToTransform);
+        }
+
+        /// <summary>
+        /// Is this url an absolute uri...with a few additional checks
+        /// </summary>
+        /// <param name="uriToInspect">uri to inspect</param>
+        /// <returns>True if it's an asbolute uri</returns>
+        public static bool IsAbsoluteUri(string uriToInspect)
+        {
+            return uriToInspect.StartsWith("www", StringComparison.OrdinalIgnoreCase) || Uri.TryCreate(uriToInspect, UriKind.Absolute, out _);
+        }
+    }
+}
