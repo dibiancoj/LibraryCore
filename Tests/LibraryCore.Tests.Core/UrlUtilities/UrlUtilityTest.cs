bff2d25ede4478aa8fbbeb6d2bb2fcc0374fbd73
Jason <dibiancoj@gmail.com>
2021-06-29 17:25:44 -0400

    uri util

---
+++ Tests/LibraryCore.Tests.Core/UrlUtilities/UrlUtilityTest.cs
@@ -0,0 +1,33 @@
+﻿using LibraryCore.Core.UrlUtilities;
+using System;
+using Xunit;
+
+namespace LibraryCore.Tests.Core.UrlUtilities
+{
+    public class UrlUtilityTest
+    {
+        [InlineData("http://www.google.com", "http://www.google.com", "http://www.google.com/")]
+        [InlineData("www.google.com", "http://www.google.com", "www.google.com")]
+        [InlineData("/test/doctor", "http://www.google.com", "http://www.google.com/test/doctor")]
+        [InlineData("test/doctor", "http://www.google.com", "http://www.google.com/test/doctor")]
+        [InlineData("test/doctor?id=5", "http://www.google.com", "http://www.google.com/test/doctor?id=5")]
+        [Theory]
+        public void UrlMakeAbsolute(string urlToInspect, string baseUrl, string expectedResult)
+        {
+            Assert.Equal(expectedResult, UrlUtility.MakeRelativeUriAbsolute(urlToInspect, new Uri(baseUrl)).ToString());
+        }
+
+        [InlineData(true, "http://www.google.com")]
+        [InlineData(true, "https://www.google.com")]
+        [InlineData(true, "www.google.com")]
+        [InlineData(false, "/test/doctor")]
+        [InlineData(false, "test/doctor")]
+        [InlineData(false, "test/doctor?id=5")]
+        [InlineData(false, "#Id=5")]
+        [Theory]
+        public void IsAbsoluteUri(bool expectIsAbsoluteUri, string urlToInspect)
+        {
+            Assert.Equal(expectIsAbsoluteUri, UrlUtility.IsAbsoluteUri(urlToInspect));
+        }
+    }
+}
