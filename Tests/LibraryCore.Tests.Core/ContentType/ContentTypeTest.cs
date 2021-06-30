002e26808dd5bb1a007510352f30ce989def404d
Jason <dibiancoj@gmail.com>
2021-06-30 11:17:27 -0400

    content types

---
+++ Tests/LibraryCore.Tests.Core/ContentType/ContentTypeTest.cs
@@ -0,0 +1,35 @@
+﻿using LibraryCore.Core.ContentType;
+using Xunit;
+
+namespace LibraryCore.Tests.Core.ContentType
+{
+    public class ContentTypeTest
+    {
+        [InlineData("application/json", ContentTypeLookup.JsonContentType)]
+        [InlineData("text/html", ContentTypeLookup.HtmlContentType)]
+        [InlineData("text/plain", ContentTypeLookup.TextPlainContentType)]
+        [Theory(DisplayName = "Verify Correct Content Type")]
+        public void VerifyContentTypes(string expectedValue, string testValue)
+        {
+            Assert.Equal(expectedValue, testValue);
+        }
+
+        [Fact]
+        public void JsonMedicaType()
+        {
+            Assert.Equal(ContentTypeLookup.JsonContentType, ContentTypeLookup.JsonMediaType.MediaType);
+        }
+
+        [Fact]
+        public void HtmlMedicaType()
+        {
+            Assert.Equal(ContentTypeLookup.HtmlContentType, ContentTypeLookup.HtmlMediaType.MediaType);
+        }
+
+        [Fact]
+        public void TextMedicaType()
+        {
+            Assert.Equal(ContentTypeLookup.TextPlainContentType, ContentTypeLookup.TextMediaType.MediaType);
+        }
+    }
+}
