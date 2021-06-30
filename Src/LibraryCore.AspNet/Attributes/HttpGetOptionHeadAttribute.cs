4323567382b59e8297d368d9df642038e1fed0d9
Jason <dibiancoj@gmail.com>
2021-06-30 12:16:53 -0400

    start

---
+++ Src/LibraryCore.AspNet/Attributes/HttpGetOptionHeadAttribute.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Routing;
+using System;
+using System.Collections.Immutable;
+using System.Net.Http;
+
+namespace LibraryCore.AspNet.Attributes
+{
+    /// <summary>
+    /// Allow either an http get or an http option (for the load balancer)
+    /// </summary>
+    public class HttpGetOptionHeadAttribute : HttpMethodAttribute
+    {
+        private static IImmutableList<string> SupportedMethodTypes { get; } = new[]
+        {
+            HttpMethod.Get.Method.ToUpper(),
+            HttpMethod.Options.Method.ToUpper(),
+            HttpMethod.Head.Method.ToUpper()
+        }.ToImmutableList();
+
+        public HttpGetOptionHeadAttribute()
+            : base(SupportedMethodTypes)
+        {
+        }
+
+        public HttpGetOptionHeadAttribute(string template)
+            : base(SupportedMethodTypes, template)
+        {
+            //template should be not be null in this overload. Throw like all the rest of the HttpMethodAttribute implementations by Microsoft.
+            if (template == null)
+            {
+                throw new ArgumentNullException(nameof(template));
+            }
+        }
+    }
+}
---
+++ Tests/LibraryCore.Tests.AspNet/Attributes/HttpGetOptionHeadAttributeTest.cs
@@ -0,0 +1,37 @@
+﻿using LibraryCore.AspNet.Attributes;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace LibraryCore.Tests.AspNet.Attributes
+{
+    public class HttpGetOptionHeadAttributeTest
+    {
+        //template overload constructor
+        [InlineData("TemplateValue")]
+
+        //no parameter constructor 
+        [InlineData(null)]
+        [Theory]
+        public void VerifyCorrectHttpMethodsAreBeingUsed(string templateValue)
+        {
+            var supportedHttpTypes = templateValue == null ?
+                                        new HttpGetOptionHeadAttribute().HttpMethods :
+                                        new HttpGetOptionHeadAttribute(templateValue).HttpMethods;
+
+            Assert.Equal(3, supportedHttpTypes.Count());
+            Assert.Contains(supportedHttpTypes, x => x == "GET");
+            Assert.Contains(supportedHttpTypes, x => x == "OPTIONS");
+            Assert.Contains(supportedHttpTypes, x => x == "HEAD");
+        }
+
+        /// <summary>
+        /// with template
+        /// </summary>
+        [Fact]
+        public void VerifyThrowsOnNullTemplate()
+        {
+            Assert.Throws<ArgumentNullException>(() => new HttpGetOptionHeadAttribute(null).HttpMethods);
+        }
+    }
+}
