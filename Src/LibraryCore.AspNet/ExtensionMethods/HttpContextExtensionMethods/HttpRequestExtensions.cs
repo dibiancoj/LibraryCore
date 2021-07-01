9ca23cca9fe11d6f0cb4c95618337a1bb27e981d
Jason <dibiancoj@gmail.com>
2021-07-01 17:38:30 -0400

    ext methods

---
+++ Src/LibraryCore.AspNet/ExtensionMethods/HttpContextExtensionMethods/HttpRequestExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace LibraryCore.AspNet.ExtensionMethods.HttpContextExtensionMethods
+{
+    public static class HttpRequestExtensions
+    {
+        //header collection doesn't throw when it doesn't exist.
+        public static bool IsAjaxRequest(this HttpRequest request) => request.Headers["X-Requested-With"] == "XMLHttpRequest";
+    }
+}
