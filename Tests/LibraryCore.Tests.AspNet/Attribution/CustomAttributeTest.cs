0b9fc38bf5877ee97406f9dbbcea7858b48fe3bf
Jason <dibiancoj@gmail.com>
2021-07-01 17:34:23 -0400

    attribution

---
+++ Tests/LibraryCore.Tests.AspNet/Attribution/CustomAttributeTest.cs
@@ -0,0 +1,51 @@
+﻿using LibraryCore.AspNet.Attribution;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using System;
+using Xunit;
+
+namespace LibraryCore.Tests.AspNet.Attribution
+{
+    public class CustomAttributeTest
+    {
+
+        #region Framework
+
+        private class MyTestAttribute : Attribute
+        {
+        }
+
+        private class TestController : Controller
+        {
+            public IActionResult Index() { throw new NotImplementedException(); }
+
+            [MyTest]
+            public IActionResult IndexWithAttribute() { throw new NotImplementedException(); }
+        }
+
+        #endregion
+
+        #region Unit Tests
+
+        [Fact]
+        public void CustomAttributeIsDefined()
+        {
+            Assert.True(CustomAttributes.CustomAttributeIsDefined<MyTestAttribute>(new ControllerActionDescriptor { MethodInfo = typeof(TestController).GetMethod(nameof(TestController.IndexWithAttribute)) }));
+        }
+
+        [Fact]
+        public void CustomAttributeIsNotDefined()
+        {
+            Assert.False(CustomAttributes.CustomAttributeIsDefined<MyTestAttribute>(new ControllerActionDescriptor { MethodInfo = typeof(TestController).GetMethod(nameof(TestController.Index)) }));
+        }
+
+        [Fact]
+        public void CustomAttributeNullControllerActionDescriptor()
+        {
+            Assert.False(CustomAttributes.CustomAttributeIsDefined<MyTestAttribute>(null));
+        }
+
+        #endregion
+
+    }
+}
