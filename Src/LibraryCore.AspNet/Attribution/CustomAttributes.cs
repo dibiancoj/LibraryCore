433ce22e1a601b3f7c9694696e965076d2ffe67b
Jason <dibiancoj@gmail.com>
2021-07-01 17:34:23 -0400

    attribution

---
+++ Src/LibraryCore.AspNet/Attribution/CustomAttributes.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using System;
+
+namespace LibraryCore.AspNet.Attribution
+{
+    public static class CustomAttributes
+    {
+        public static bool CustomAttributeIsDefined<TAttribute>(ActionDescriptor actionDescriptor)
+            where TAttribute : Attribute
+        {
+            var castedAction = actionDescriptor as ControllerActionDescriptor;
+
+            return castedAction == null ?
+                    false :
+                    castedAction.MethodInfo.IsDefined(typeof(TAttribute), true);
+        }
+    }
+}
