b735bca046ce6a9146bf14bd1f0006f15a11e160
Jason <dibiancoj@gmail.com>
2021-07-01 19:08:39 -0400

    render service

---
+++ Src/LibraryCore.AspNet/Render/IRenderService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Routing;
+using System.Threading.Tasks;
+
+namespace LibraryCore.AspNet.Render
+{
+    public interface IRenderService
+    {
+        /// <summary>
+        /// Render a view or partial view to a string when you don't have a model
+        /// </summary>
+        /// <param name="fullpathToViewOrPartial">view or partial name to render. Use the full path of the view ie: ~/Areas/Patient/Views/Home/Index.cshtml</param>
+        /// <returns>Task which will return a string</returns>
+        Task<string> RenderToStringAsync(string fullpathToViewOrPartial);
+
+        /// <summary>
+        /// Render a view or partial view to a string
+        /// </summary>
+        /// <param name="fullpathToViewOrPartial">view or partial name to render. Use the full path of the view ie: ~/Areas/Patient/Views/Home/Index.cshtml</param>
+        /// <param name="model">model to render with. Null if no model</param>
+        /// <returns>Task which will return a string</returns>
+        Task<string> RenderToStringAsync(string fullpathToViewOrPartial, object model);
+
+        /// <summary>
+        /// Render a view or partial view to a string
+        /// </summary>
+        /// <param name="fullpathToViewOrPartial">view or partial name to render. Use the full path of the view ie: ~/Areas/Patient/Views/Home/Index.cshtml</param>
+        /// <param name="model">model to render with. Null if no model</param>
+        /// <param name="modelStateDictionary">Model state</param>
+        /// <param name="routeData">Route data if applicable</param>
+        /// <param name="actionDescriptor">action descriptor if applicable</param>
+        /// <returns>Task which will return a string</returns>
+        Task<string> RenderToStringAsync(string fullpathToViewOrPartial, object? model, ModelStateDictionary? modelStateDictionary, RouteData? routeData, ActionDescriptor? actionDescriptor);
+    }
+}
