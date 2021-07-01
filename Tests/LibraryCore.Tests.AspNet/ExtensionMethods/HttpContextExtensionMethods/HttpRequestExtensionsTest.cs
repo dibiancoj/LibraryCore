a20999b7803e6f799fef8ca6b4631f79fb89da22
Jason <dibiancoj@gmail.com>
2021-07-01 17:38:30 -0400

    ext methods

---
+++ Tests/LibraryCore.Tests.AspNet/ExtensionMethods/HttpContextExtensionMethods/HttpRequestExtensionsTest.cs
@@ -0,0 +1,48 @@
+﻿using LibraryCore.AspNet.ExtensionMethods.HttpContextExtensionMethods;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace LibraryCore.Tests.AspNet.ExtensionMethods.HttpContextExtensionMethods
+{
+    public class HttpRequestExtensionsTest
+    {
+
+        #region Framework
+
+        public class MyController : Controller
+        {
+        }
+
+        #endregion
+
+        #region Unit Tests
+
+        [Fact(DisplayName = "Is not ajax request")]
+        public void IsAjaxWhenRequestIsNotAjaxCall()
+        {
+            var httpContext = new DefaultHttpContext();
+
+            Assert.False(httpContext.Request.IsAjaxRequest());
+        }
+
+        [Fact(DisplayName = "Is an ajax request")]
+        public void IsAjaxWhenRequestIsAjaxCall()
+        {
+            var controller = new MyController
+            {
+                ControllerContext = new ControllerContext
+                {
+                    HttpContext = new DefaultHttpContext()
+                }
+            };
+
+            controller.ControllerContext.HttpContext.Request.Headers["X-Requested-With"] = "XMLHttpRequest";
+
+            Assert.True(controller.Request.IsAjaxRequest());
+        }
+
+        #endregion
+
+    }
+}
