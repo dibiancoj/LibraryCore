efb0658374838e93971f2a28f157cce52cc6161c
Jason <dibiancoj@gmail.com>
2021-07-01 22:50:25 -0400

    accounting periods

---
+++ Src/LibraryCore.Core/DataTypes/AccountingPeriod.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace LibraryCore.Core.DataTypes
+{
+    public class AccountingPeriod
+    {
+
+        #region Constructors
+
+        public AccountingPeriod(DateTime accountingPeriodToParse)
+        {
+            Month = ValidateMonthOrThrow(accountingPeriodToParse.Month);
+            Year = ValidateYearOrThrow(accountingPeriodToParse.Year);
+        }
+
+        public AccountingPeriod(int month, int year)
+        {
+            Month = ValidateMonthOrThrow(month);
+            Year = ValidateYearOrThrow(year);
+        }
+
+        public AccountingPeriod(int fullAccountingPeriod)
+        {
+            var temp = TryParseFullAccountingPeriod(fullAccountingPeriod);
+
+            Year = ValidateYearOrThrow(Convert.ToInt32(new string(temp.Slice(0, 4))));
+            Month = ValidateMonthOrThrow(Convert.ToInt32(new string(temp.Slice(4, 2))));
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int Month { get; }
+        public int Year { get; }
+
+        #endregion
+
+        #region Operators
+
+        public static AccountingPeriod operator +(AccountingPeriod a, int b)
+        {
+            return new AccountingPeriod(a.ToDateTime().AddMonths(b));
+        }
+
+        public static AccountingPeriod operator -(AccountingPeriod a, int b)
+        {
+            return new AccountingPeriod(a.ToDateTime().AddMonths(-b));
+        }
+
+        #endregion
+
+        #region Methods
+
+        public DateTime ToDateTime() => new(Year, Month, 1);
+
+        public int ToFullAccountingPeriod() => Convert.ToInt32($"{Year}{Month.ToString("D2")}");
+
+        #endregion
+
+        #region Private Validation Methods
+
+        private static ReadOnlySpan<char> TryParseFullAccountingPeriod(int fullAccountingPeriod)
+        {
+            var temp = fullAccountingPeriod.ToString().AsSpan();
+
+            return temp.Length != 6 ?
+                 throw new Exception($"Invalid Format. Expected YYYYMM - Got {fullAccountingPeriod}") :
+                 temp;
+        }
+
+        private static int ValidateYearOrThrow(int yearToValidate)
+        {
+            return yearToValidate.ToString().Length != 4 ?
+                throw new Exception($"Invalid Year Format. Expected YYYY - Got {yearToValidate}") :
+                yearToValidate;
+        }
+
+        private static int ValidateMonthOrThrow(int monthToValidate)
+        {
+            return monthToValidate < 1 || monthToValidate > 12 ?
+                throw new Exception($"Invalid Month Format. Expected MM - Got {monthToValidate}") :
+                monthToValidate;
+        }
+
+        #endregion
+
+    }
+}
---
+++ Tests/LibraryCore.Tests.Core/DataTypes/AccountingPeriodTest.cs
@@ -0,0 +1,86 @@
+﻿using LibraryCore.Core.DataTypes;
+using System;
+using Xunit;
+
+namespace LibraryCore.Tests.Core.DataTypes
+{
+    public class AccountingPeriodTest
+    {
+
+        [Fact]
+        public void BuildPeriodFromDate()
+        {
+            var temp = new AccountingPeriod(new DateTime(2020, 5, 1));
+
+            Assert.Equal(2020, temp.Year);
+            Assert.Equal(5, temp.Month);
+            Assert.Equal(202005, temp.ToFullAccountingPeriod());
+            Assert.Equal(new DateTime(2020, 5, 1), temp.ToDateTime());
+        }
+
+        [Fact]
+        public void BuildPeriodFromNumbers()
+        {
+            var temp = new AccountingPeriod(5, 2020);
+
+            Assert.Equal(2020, temp.Year);
+            Assert.Equal(5, temp.Month);
+            Assert.Equal(202005, temp.ToFullAccountingPeriod());
+            Assert.Equal(new DateTime(2020, 5, 1), temp.ToDateTime());
+        }
+
+        [Fact]
+        public void BuildPeriodFromNumbersWithInvalidMonth()
+        {
+            Assert.Throws<Exception>(() => new AccountingPeriod(13, 2020));
+        }
+
+        [Fact]
+        public void BuildPeriodFromNumbersWithInvalidYear()
+        {
+            Assert.Throws<Exception>(() => new AccountingPeriod(05, 1));
+        }
+
+        [Fact]
+        public void BuildPeriodFromFullAccountingPeriod()
+        {
+            var temp = new AccountingPeriod(202005);
+
+            Assert.Equal(2020, temp.Year);
+            Assert.Equal(5, temp.Month);
+            Assert.Equal(202005, temp.ToFullAccountingPeriod());
+            Assert.Equal(new DateTime(2020, 5, 1), temp.ToDateTime());
+        }
+
+        [Fact]
+        public void BuildPeriodFromFullAccountingPeriodWithInvalidMonth()
+        {
+            Assert.Throws<Exception>(() => new AccountingPeriod(202015));
+        }
+
+        [Fact]
+        public void BuildPeriodFromFullAccountingPeriodWithInvalidYear()
+        {
+            Assert.Throws<Exception>(() => new AccountingPeriod(05012));
+        }
+
+        [InlineData(202001, 1, 202002)] //1 month
+        [InlineData(202001, 3, 202004)] //3 month
+        [InlineData(202005, 12, 202105)] //full year
+        [Theory]
+        public void IncrementPeriods(int accountPeriod, int periodsToIncrement, int expectedResult)
+        {
+            Assert.Equal(expectedResult, (new AccountingPeriod(accountPeriod) + periodsToIncrement).ToFullAccountingPeriod());
+        }
+
+        [InlineData(202002, 1, 202001)] //-1 month
+        [InlineData(202003, 3, 201912)] //-3 month
+        [InlineData(202105, 12, 202005)] //minus full year
+        [Theory]
+        public void SubtractPeriods(int accountPeriod, int periodsToIncrement, int expectedResult)
+        {
+            Assert.Equal(expectedResult, (new AccountingPeriod(accountPeriod) - periodsToIncrement).ToFullAccountingPeriod());
+        }
+
+    }
+}
