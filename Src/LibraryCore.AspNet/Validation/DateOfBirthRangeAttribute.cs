7b817c9332cf4c36a19e43c1698fc5db59852863
Jason <dibiancoj@gmail.com>
2021-07-01 20:02:33 -0400

    validation

---
+++ Src/LibraryCore.AspNet/Validation/DateOfBirthRangeAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryCore.AspNet.Validation
+{
+    public class DateOfBirthRangeAttribute : RangeAttribute
+    {
+        // We are working around a Data Annotation / DateTime limitation so we can establish a reasonable DateTime range.
+        public DateOfBirthRangeAttribute()
+            : base(typeof(DateTime), DateTime.Now.AddYears(-200).ToShortDateString(), DateTime.Now.AddYears(1).ToShortDateString())
+        {
+        }
+    }
+}
---
+++ Tests/LibraryCore.Tests.AspNet/Validation/DateOfBirthValueValidationTest.cs
@@ -0,0 +1,20 @@
+﻿using LibraryCore.AspNet.Validation;
+using System;
+using Xunit;
+
+namespace LibraryCore.Tests.AspNet.Validation
+{
+    public class DateOfBirthValueValidationTest
+    {
+        [InlineData(-300, false)]
+        [InlineData(0, true)]
+        [InlineData(1, false)]
+        [InlineData(2, false)]
+        [InlineData(-10, true)]
+        [Theory]
+        public void DateOfBirthValidation(int yearsToAddToTest, bool isValidExpectedResult)
+        {
+            Assert.Equal(isValidExpectedResult, new DateOfBirthRangeAttribute().IsValid(DateTime.Now.AddYears(yearsToAddToTest)));
+        }
+    }
+}
