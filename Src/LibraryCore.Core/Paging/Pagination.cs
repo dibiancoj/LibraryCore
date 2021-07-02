72e0bf716699d3058d49385e4482d0631bd42e04
Jason DiBianco <dibiancoj@gmail.com>
2021-07-02 10:01:33 -0400

    more stuff (#21)
    * paging
    
    * excel stuff
    
    * more
    
    Co-authored-by: dibiancj@mskcc.org <dibiancj@mskcc.org>

---
+++ Src/LibraryCore.Core/Paging/Pagination.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LibraryCore.Core.Paging
+{
+    /// <summary>
+    /// Helps page data for a data set
+    /// </summary>
+    public static class Pagination
+    {
+        //** Paging of the data is in IOrderedEnumerable Extension Methods **
+
+        /// <summary>
+        /// Calculates Total Number Of Pages In This Grid (Property Of Total Above - Need Additional Data To Calculate)
+        /// </summary>
+        /// <param name="howManyTotalRecordsInDataSet">Total Number Of Records (Not Just This Page But In The Entire RecordSet)</param>
+        /// <param name="howManyRecordsPerPage">How Many Records Per Page</param>
+        /// <returns>Number Of Pages</returns>
+        public static int CalculateTotalPages(int howManyTotalRecordsInDataSet, int howManyRecordsPerPage)
+        {
+            //calculate how many pages we have
+            double Conversion = ((double)howManyTotalRecordsInDataSet / (double)howManyRecordsPerPage);
+
+            //do we have an even amount
+            if ((Conversion % 1) == 0)
+            {
+                //we have an even amount
+                return Convert.ToInt32(Conversion);
+            }
+
+            //we have an uneven amount...so grab the floor then add 1
+            return Convert.ToInt32(Math.Floor(Conversion)) + 1;
+        }
+
+    }
+}
---
+++ Tests/LibraryCore.Tests.Core/ExcelUtilities/ExcelUtilityTest.cs
@@ -0,0 +1,39 @@
+﻿using LibraryCore.Core.ExcelUtilities;
+using Xunit;
+
+namespace LibraryCore.Tests.Core.ExcelUtilities
+{
+    public class ExcelUtilityTest
+    {
+        #region Column Name To Int
+
+        /// <summary>
+        /// Test column name to number (for excel)
+        /// </summary>
+        [Fact]
+        public void ExcelToolsColumnNameToIntTest1()
+        {
+            Assert.Equal(1, ExcelUtility.ColumnLetterToColumnIndex("A"));
+            Assert.Equal(27, ExcelUtility.ColumnLetterToColumnIndex("AA"));
+            Assert.Equal(4, ExcelUtility.ColumnLetterToColumnIndex("D"));
+
+        }
+
+        #endregion
+
+        #region Column Number To Letter
+
+        /// <summary>
+        /// Test column number to letter (for excel)
+        /// </summary>
+        [Fact]
+        public void ExcelToolsColumnNumberToLetterTest1()
+        {
+            Assert.Equal("AA", ExcelUtility.ColumnIndexToColumnLetter(27));
+            Assert.Equal("A", ExcelUtility.ColumnIndexToColumnLetter(1));
+            Assert.Equal("D", ExcelUtility.ColumnIndexToColumnLetter(4));
+        }
+
+        #endregion
+    }
+}
