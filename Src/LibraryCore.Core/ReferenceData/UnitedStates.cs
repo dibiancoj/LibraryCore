0870410bf3a470d4920a5aee0e4f2eb76d56ee18
Jason DiBianco <dibiancoj@gmail.com>
2021-07-02 23:10:29 -0400

    united states - states (#29)
    * united states - states
    
    * ignore the deserializer null check.

---
+++ Src/LibraryCore.Core/ReferenceData/UnitedStates.cs
@@ -0,0 +1,30 @@
+﻿using LibraryCore.Core.Properties;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace LibraryCore.Core.ReferenceData
+{
+    public static class UnitedStates
+    {
+        private record TempStateStorageModel
+        {
+            [JsonPropertyName("states")]
+            public IEnumerable<UnitedStatesStateModel> States { get; init; } = null!;
+        }
+
+        public record UnitedStatesStateModel
+        {
+            [JsonPropertyName("id")]
+            public string Id { get; init; } = null!;
+
+            [JsonPropertyName("description")]
+            public string Description { get; init; } = null!;
+        }
+
+        public static IEnumerable<UnitedStatesStateModel> StateListing()
+        {
+            return JsonSerializer.Deserialize<TempStateStorageModel>(Resources.UnitedState_StateListing)!.States;
+        }
+    }
+}
---
+++ Tests/LibraryCore.Tests.Core/ReferenceData/UnitedStatesTest.cs
@@ -0,0 +1,19 @@
+﻿using LibraryCore.Core.ReferenceData;
+using System.Linq;
+using Xunit;
+
+namespace LibraryCore.Tests.Core.ReferenceData
+{
+    public class UnitedStatesTest
+    {
+        [Fact]
+        public void UnitedStateListingTest()
+        {
+            var results = UnitedStates.StateListing();
+
+            Assert.Equal(52, results.Count());
+            Assert.Single(results, x => x.Id == "NY" && x.Description == "New York");
+            Assert.Single(results, x => x.Id == "NJ" && x.Description == "New Jersey");
+        }
+    }
+}
