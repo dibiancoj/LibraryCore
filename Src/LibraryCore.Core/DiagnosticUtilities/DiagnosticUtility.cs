966deaf8090294a90b50fd780c132e943a4d9480
Jason DiBianco <dibiancoj@gmail.com>
2021-07-03 18:04:02 -0400

    spin wait until (#33)

---
+++ Src/LibraryCore.Core/DiagnosticUtilities/DiagnosticUtility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace LibraryCore.Core.DiagnosticUtilities
+{
+    public static class DiagnosticUtility
+    {
+        /// <summary>
+        /// Run a spin and wait until the timespan passed in has expired
+        /// </summary>
+        /// <param name="timeSpanToSpinUntil">Time span to spin until. If you pass in 5 seconds then we will spin until that expires</param>
+        public static void SpinWaitUntilTimespan(TimeSpan timeSpanToSpinUntil)
+        {
+            //figure out the time we need to spin until
+            var spinUntil = DateTime.Now.Add(timeSpanToSpinUntil);
+
+            //Spin until we are past the spin until variable
+            SpinWait.SpinUntil(() => DateTime.Now > spinUntil);
+        }
+    }
+}
---
+++ Tests/LibraryCore.Tests.Core/DiagnosticUtilities/DiagnosticUtilityTest.cs
@@ -0,0 +1,25 @@
+﻿using LibraryCore.Core.DiagnosticUtilities;
+using System;
+using Xunit;
+
+namespace LibraryCore.Tests.Core.DiagnosticUtilities
+{
+    public class DiagnosticUtilityTest
+    {
+        [Fact]
+        public void SpinWaitUntilTimespanTest1()
+        {
+            //grab now
+            DateTime now = DateTime.Now;
+
+            //spin until then
+            DiagnosticUtility.SpinWaitUntilTimespan(new TimeSpan(0, 0, 2));
+
+            //grab the current time
+            var timeNow = DateTime.Now.Subtract(now).Seconds;
+
+            //give a little bit of a buffer to finish..so check 3 to 3.75 seconds
+            Assert.True(timeNow >= 2 && timeNow < 2.75);
+        }
+    }
+}
