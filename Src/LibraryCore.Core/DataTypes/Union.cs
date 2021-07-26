01eed16846d1b20cecae982135d675d52fcf11ce
dibiancj@mskcc.org <dibiancj@mskcc.org>
2021-07-26 12:05:35 -0400

    base union type

---
+++ Src/LibraryCore.Core/DataTypes/Union.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace LibraryCore.Core.DataTypes
+{
+
+    /// <summary>
+    /// Union type between 2 different data types
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    public class Union<T1, T2>
+    {
+        /// <summary>
+        /// Create a union with the first type
+        /// </summary>
+        /// <param name="t1Value">Value of the T1 value</param>
+        public Union(T1 t1Value)
+        {
+            UnionType = typeof(T1);
+            TValue = t1Value;
+        }
+
+        /// <summary>
+        /// Create a union with the second type
+        /// </summary>
+        /// <param name="t2Value"></param>
+        public Union(T2 t2Value)
+        {
+            UnionType = typeof(T2);
+            TValue = t2Value;
+        }
+
+        private Type UnionType { get; init; }
+        private dynamic? TValue { get; init; }
+
+        /// <summary>
+        /// Returns true if the union contains a value of type T
+        /// </summary>
+        /// <remarks>The type of T must exactly match the type</remarks>
+        public bool Is<T>() => typeof(T) == UnionType;
+
+        /// <summary>
+        /// Returns the union value cast to the given type.
+        /// </summary>
+        /// <remarks>Returns the value for the given type if it matches T. If the value does not match the the default value of T is returned</remarks>
+        public T? As<T>()
+        {
+            return Is<T>() ?
+                     TValue as dynamic :
+                     default;
+        }
+
+    }
+}
---
+++ Tests/LibraryCore.Tests.Core/DataTypes/UnionTest.cs
@@ -0,0 +1,100 @@
+﻿using LibraryCore.Core.DataTypes;
+using System;
+using Xunit;
+
+namespace LibraryCore.Tests.Core.DataTypes
+{
+    public class UnionTest
+    {
+        [Fact]
+        public void UnionTypeWithObject()
+        {
+            var unionToTest = new Union<string, DateTime>("test");
+
+            Assert.True(unionToTest.Is<string>());
+            Assert.False(unionToTest.Is<DateTime>());
+            Assert.False(unionToTest.Is<bool>());
+
+            Assert.Equal("test", unionToTest.As<string>());
+            Assert.Equal(default, unionToTest.As<DateTime>());
+            Assert.Equal(default, unionToTest.As<bool>());
+        }
+
+        [Fact]
+        public void UnionTypeWithStruct()
+        {
+            var dateToUse = DateTime.Now;
+
+            var unionToTest = new Union<string, DateTime>(dateToUse);
+
+            Assert.False(unionToTest.Is<string>());
+            Assert.True(unionToTest.Is<DateTime>());
+            Assert.False(unionToTest.Is<bool>());
+
+            Assert.Equal(default, unionToTest.As<string>());
+            Assert.Equal(dateToUse, unionToTest.As<DateTime>());
+            Assert.Equal(default, unionToTest.As<bool>());
+        }
+
+        [Fact]
+        public void UnionTypeWithNullableObjectThatIsNull()
+        {
+            var unionToTest = new Union<bool?, DateTime>(null);
+
+            Assert.True(unionToTest.Is<bool?>());
+            Assert.False(unionToTest.Is<DateTime>());
+
+            Assert.Equal(default, unionToTest.As<string>());
+            Assert.False(unionToTest.As<bool?>().HasValue);
+        }
+
+        [Fact]
+        public void UnionTypeWithNullableObjectThatHasAValue()
+        {
+            var unionToTest = new Union<bool, DateTime>(true);
+
+            Assert.True(unionToTest.Is<bool>());
+            Assert.False(unionToTest.Is<DateTime>());
+
+            Assert.Equal(default, unionToTest.As<string>());
+            Assert.True(unionToTest.As<bool>());
+        }
+
+        #region Realistic Example
+
+        public record ValidPasswordResponse(string UserName);
+        public record InvalidPasswordResponse(string ErrorMessage);
+        
+        [Fact]
+        public void WithValidPasswordResponse()
+        {
+            var unionToTest = new Union<ValidPasswordResponse, InvalidPasswordResponse>(new ValidPasswordResponse("User1"));
+
+            if (unionToTest.Is<ValidPasswordResponse>())
+            {
+                Assert.Equal("User1", unionToTest.As<ValidPasswordResponse>().UserName);
+            }
+            else 
+            {
+                throw new Exception("Unit test should not hit this exception");
+            }
+        }
+
+        [Fact]
+        public void WithInValidPasswordResponse()
+        {
+            var unionToTest = new Union<ValidPasswordResponse, InvalidPasswordResponse>(new InvalidPasswordResponse("Invalid user name and or password"));
+
+            if (unionToTest.Is<ValidPasswordResponse>())
+            {
+                throw new Exception("Unit test should not hit this exception");
+            }
+            else
+            {
+                Assert.Equal("Invalid user name and or password", unionToTest.As<InvalidPasswordResponse>().ErrorMessage);
+            }
+        }
+
+        #endregion
+    }
+}
