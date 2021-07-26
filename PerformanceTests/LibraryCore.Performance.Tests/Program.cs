5c1c97f45d730b403b295326280ea3c85c04d069
Jason DiBianco <dibiancoj@gmail.com>
2021-07-26 13:51:09 -0400

    perf tests (#41)
    Co-authored-by: dibiancj@mskcc.org <dibiancj@mskcc.org>

---
+++ PerformanceTests/LibraryCore.Performance.Tests/Program.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Running;
+using LibraryCore.Performance.Tests.Tests;
+using System;
+
+namespace LibraryCore.Performance.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _ = BenchmarkRunner.Run<UnionTypePerfTest>();
+
+            Console.WriteLine("Performance Test Complete. Press Any Key To Exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PerformanceTests/LibraryCore.Performance.Tests/Tests/UnionTypePerfTest.cs
@@ -0,0 +1,105 @@
+﻿using BenchmarkDotNet.Attributes;
+using LibraryCore.Core.DataTypes.Unions;
+using System;
+
+namespace LibraryCore.Performance.Tests.Tests
+{
+    [SimpleJob]
+    [MemoryDiagnoser]
+    public class UnionTypePerfTest
+    {
+        [GlobalSetup]
+        public void Init()
+        {
+            DateToUse = DateTime.Now;
+        }
+
+        private DateTime DateToUse { get; set; }
+
+        [Benchmark(Baseline = true)]
+        public string DynamicCurrentImplementation()
+        {
+            var unionToTest = new Union<string, DateTime>(DateToUse);
+
+            if (unionToTest.Is<string>())
+            {
+                return unionToTest.As<string>();
+            }
+            else if (unionToTest.Is<DateTime>())
+            {
+                return unionToTest.As<DateTime>().ToString();
+            }
+
+            throw new Exception("Invalid");
+        }
+
+        [Benchmark]
+        public string ObjectBased()
+        {
+            var unionToTest = new UnionNew<string, DateTime>(DateToUse);
+
+            if (unionToTest.Is<string>())
+            {
+                return unionToTest.As<string>();
+            }
+            else if (unionToTest.Is<DateTime>())
+            {
+                return unionToTest.As<DateTime>().ToString();
+            }
+
+            throw new Exception("Invalid");
+        }
+    }
+
+    #region New Implementation
+
+    public class UnionNew<T1, T2> : UnionBaseNew
+    {
+        /// <summary>
+        /// Create a union with the first type
+        /// </summary>
+        /// <param name="t1Value">Value of the T1 value</param>
+        public UnionNew(T1 t1Value)
+        {
+            UnionType = typeof(T1);
+            TValue = t1Value;
+        }
+
+        /// <summary>
+        /// Create a union with the second type
+        /// </summary>
+        /// <param name="t2Value"></param>
+        public UnionNew(T2 t2Value)
+        {
+            UnionType = typeof(T2);
+            TValue = t2Value;
+        }
+    }
+
+    public abstract class UnionBaseNew
+    {
+        protected Type UnionType { get; init; }
+        protected object TValue { get; init; }
+
+        /// <summary>
+        /// Returns true if the union contains a value of type T
+        /// </summary>
+        /// <remarks>The type of T must exactly match the type</remarks>
+        public bool Is<T>() => typeof(T) == UnionType;
+
+        /// <summary>
+        /// Returns the union value cast to the given type.
+        /// </summary>
+        /// <remarks>Returns the value for the given type if it matches T. If the value does not match the the default value of T is returned</remarks>
+        public T As<T>()
+        {
+            return Is<T>() ?
+                     TValue as dynamic :
+                     default(T);
+        }
+
+    }
+
+    #endregion
+
+}
