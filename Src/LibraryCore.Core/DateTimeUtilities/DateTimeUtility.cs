e6827f14c4d1c4a1f5ebf58de52c41e01cb61929
Jason <dibiancoj@gmail.com>
2021-08-02 15:57:33 -0400

    quarter

--- Src/LibraryCore.Core/DateTimeUtilities/DateTimeUtility.cs
+++ Src/LibraryCore.Core/DateTimeUtilities/DateTimeUtility.cs
@@ -26,5 +26,23 @@ namespace LibraryCore.Core.DateTimeUtilities
             //return the age
             return age;
         }
+
+        /// <summary>
+        /// Figure out which quarter this time period falls in
+        /// </summary>
+        /// <param name="whichQuarterIsDateTimeIn">Date time to figure out which quarter this falls in</param>
+        /// <returns>Which Quarter 1 through 4</returns>
+        public static int QuarterIsInTimePeriod(DateTime whichQuarterIsDateTimeIn)
+        {
+            //determine which quarter by the month
+            return whichQuarterIsDateTimeIn.Month switch
+            {
+                1 or 2 or 3 => 1,
+                4 or 5 or 6 => 2,
+                7 or 8 or 9 => 3,
+                10 or 11 or 12 => 4,
+                _ => throw new ArgumentOutOfRangeException(nameof(whichQuarterIsDateTimeIn), "Month Of WhichQuarterIsDateTimeIn Can't Be Found In Quarter Lookup"),
+            };
+        }
     }
 }
--- Tests/LibraryCore.Tests.Core/DateTimeUtilities/DateTimeUtilityTest.cs
+++ Tests/LibraryCore.Tests.Core/DateTimeUtilities/DateTimeUtilityTest.cs
@@ -6,6 +6,9 @@ namespace LibraryCore.Tests.Core.DateTimeUtilities
 {
     public class DateTimeUtilityTest
     {
+
+        #region Age
+
         [Fact]
         public void CalculateAgeTest1()
         {
@@ -29,5 +32,35 @@ namespace LibraryCore.Tests.Core.DateTimeUtilities
         {
             Assert.Equal(2, DateTimeUtility.Age(DateTime.Today.AddYears(-2).AddDays(-3)));
         }
+
+        #endregion
+
+        #region Quarter
+
+        /// <summary>
+        /// Test which quarter this date is in
+        /// </summary>
+        [Fact]
+        public void QuarterTimePeriodTest1()
+        {
+            Assert.Equal(1, DateTimeUtility.QuarterIsInTimePeriod(new DateTime(2014, 1, 1)));
+            Assert.Equal(1, DateTimeUtility.QuarterIsInTimePeriod(new DateTime(2014, 2, 1)));
+            Assert.Equal(1, DateTimeUtility.QuarterIsInTimePeriod(new DateTime(2014, 3, 1)));
+
+            Assert.Equal(2, DateTimeUtility.QuarterIsInTimePeriod(new DateTime(2014, 4, 1)));
+            Assert.Equal(2, DateTimeUtility.QuarterIsInTimePeriod(new DateTime(2014, 5, 1)));
+            Assert.Equal(2, DateTimeUtility.QuarterIsInTimePeriod(new DateTime(2014, 6, 1)));
+
+            Assert.Equal(3, DateTimeUtility.QuarterIsInTimePeriod(new DateTime(2014, 7, 1)));
+            Assert.Equal(3, DateTimeUtility.QuarterIsInTimePeriod(new DateTime(2014, 8, 1)));
+            Assert.Equal(3, DateTimeUtility.QuarterIsInTimePeriod(new DateTime(2014, 9, 1)));
+
+            Assert.Equal(4, DateTimeUtility.QuarterIsInTimePeriod(new DateTime(2014, 10, 1)));
+            Assert.Equal(4, DateTimeUtility.QuarterIsInTimePeriod(new DateTime(2014, 11, 1)));
+            Assert.Equal(4, DateTimeUtility.QuarterIsInTimePeriod(new DateTime(2014, 12, 1)));
+        }
+
+        #endregion
+
     }
 }
