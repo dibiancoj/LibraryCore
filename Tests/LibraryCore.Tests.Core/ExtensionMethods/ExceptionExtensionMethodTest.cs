463e0a7d84dbf3d32dd78fb299fcac209f1529e5
Jason DiBianco <dibiancoj@gmail.com>
2021-09-14 13:53:23 -0400

    exception history (#58)

--- Tests/LibraryCore.Tests.Core/ExtensionMethods/ExceptionExtensionMethodTest.cs
+++ Tests/LibraryCore.Tests.Core/ExtensionMethods/ExceptionExtensionMethodTest.cs
@@ -1,5 +1,6 @@
 ﻿using LibraryCore.Core.ExtensionMethods;
 using System;
+using System.Linq;
 using System.Net.Http;
 using Xunit;
 
@@ -35,6 +36,41 @@ namespace LibraryCore.Tests.Core.ExtensionMethods
 
             Assert.False(exeptionRaised.TryParse<UnitTestException>(out var tryCastException));
             Assert.Null(tryCastException);
+        }
+
+        [Fact]
+        public void SingleExceptionTree()
+        {
+            var oneException = new HttpRequestException("Test Exception");
+
+            var result = oneException.ExceptionTree().ToArray();
+
+            Assert.Single(result);
+            Assert.Equal(result.Single(), oneException);
+        }
+
+        [Fact]
+        public void MultipleExceptionsInTree()
+        {
+            var innerInnerExeption = new ArgumentNullException("Inner Inner Message Exception");
+            var innerExeption = new NullReferenceException("Inner Message Exception", innerInnerExeption);
+            var rootException = new HttpRequestException("Test Exception", innerExeption);
+
+            var result = rootException.ExceptionTree().ToList();
+
+            Assert.Equal(3, result.Count);
+
+            //root error
+            Assert.Equal(result.OfType<HttpRequestException>().Single(), rootException);
+            Assert.IsType<HttpRequestException>(result.ElementAt(0));
+
+            //inner exception of the root
+            Assert.Equal(result.OfType<NullReferenceException>().Single(), innerExeption);
+            Assert.IsType<NullReferenceException>(result.ElementAt(1));
+
+            //inner off of the inner
+            Assert.Equal(result.OfType<ArgumentNullException>().Single(), innerInnerExeption);
+            Assert.IsType<ArgumentNullException>(result.ElementAt(2));
         }
 
     }
