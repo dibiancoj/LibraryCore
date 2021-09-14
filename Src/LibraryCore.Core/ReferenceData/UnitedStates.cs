7a1a55239d50f5cd7b8165a930b418661deb7b1d
Jason <dibiancoj@gmail.com>
2021-09-14 13:23:16 -0400

    json property cleanup

--- Src/LibraryCore.Core/ReferenceData/UnitedStates.cs
+++ Src/LibraryCore.Core/ReferenceData/UnitedStates.cs
@@ -1,5 +1,6 @@
 ﻿using LibraryCore.Core.Properties;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -7,20 +8,10 @@ namespace LibraryCore.Core.ReferenceData
 {
     public static class UnitedStates
     {
-        private record TempStateStorageModel
-        {
-            [JsonPropertyName("states")]
-            public IEnumerable<UnitedStatesStateModel> States { get; init; } = null!;
-        }
+        private record TempStateStorageModel([property: JsonPropertyName("states")] IEnumerable<UnitedStatesStateModel> States);
 
-        public record UnitedStatesStateModel
-        {
-            [JsonPropertyName("id")]
-            public string Id { get; init; } = null!;
-
-            [JsonPropertyName("description")]
-            public string Description { get; init; } = null!;
-        }
+        [DebuggerDisplay("Id = {Id} | Description = {Description}")]
+        public record UnitedStatesStateModel([property: JsonPropertyName("id")] string Id, [property: JsonPropertyName("description")] string Description);
 
         public static IEnumerable<UnitedStatesStateModel> StateListing()
         {
