faaa5aee5c1dcc3c7ea807a306f1c15fdb8f7c84
Jason DiBianco <dibiancoj@gmail.com>
2021-09-21 20:00:27 -0400

    prime and composite number (#60)
    * prime number
    
    * remove using

---
+++ Src/LibraryCore.Core/MathUtilities/MathUtility.cs
@@ -0,0 +1,34 @@
+﻿namespace LibraryCore.Core.MathUtilities
+{
+    public static class MathUtility
+    {
+
+        /// <summary>
+        /// This this a prime number
+        /// </summary>
+        /// <param name="numberToCheck">number to check</param>
+        /// <returns>True if a prime number</returns>
+        public static bool IsPrimeNumber(int numberToCheck)
+        {
+            var goToNumber = numberToCheck / 2;
+
+            for (var i = 2; i <= goToNumber; i++)
+            {
+                if (numberToCheck % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Is it a composite number ie: not a prime number
+        /// </summary>
+        /// <param name="numberToCheck">number to check</param>
+        /// <returns>True if a composite number</returns>
+        public static bool IsCompositeNumber(int numberToCheck) => !IsPrimeNumber(numberToCheck);
+
+    }
+}
---
+++ Tests/LibraryCore.Tests.Core/MathUtilities/MathUtilityTest.cs
@@ -0,0 +1,26 @@
+﻿using LibraryCore.Core.MathUtilities;
+using Xunit;
+
+namespace LibraryCore.Tests.Core.MathUtilities
+{
+    public class MathUtilityTest
+    {
+        [InlineData(false, 6)]
+        [InlineData(true, 5)]
+        [InlineData(false, 4)]
+        [InlineData(true, 3)]
+        [InlineData(true, 2)]
+        [InlineData(true, 1)]
+        [Theory]
+        public void IsPrimeNumber(bool isPrimeNumberExpectedResult, int numberToCheck) => Assert.Equal(isPrimeNumberExpectedResult, MathUtility.IsPrimeNumber(numberToCheck));
+
+        [InlineData(true, 6)]
+        [InlineData(false, 5)]
+        [InlineData(true, 4)]
+        [InlineData(false, 3)]
+        [InlineData(false, 2)]
+        [InlineData(false, 1)]
+        [Theory]
+        public void IsCompositeNumber(bool isCompositeNumberExpectedResult, int numberToCheck) => Assert.Equal(isCompositeNumberExpectedResult, MathUtility.IsCompositeNumber(numberToCheck));
+    }
+}
