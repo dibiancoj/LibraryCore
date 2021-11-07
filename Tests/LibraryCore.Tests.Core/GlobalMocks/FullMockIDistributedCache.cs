22d0ddf5a9a39b9571dfce2c4c9804256e1646e2
Jason DiBianco <dibiancoj@gmail.com>
2021-11-07 11:11:54 -0500

    cleanup the mock distributed cache (#70)

--- Tests/LibraryCore.Tests.Core/GlobalMocks/FullMockIDistributedCache.cs
+++ Tests/LibraryCore.Tests.Core/GlobalMocks/FullMockIDistributedCache.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Caching.Distributed;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,21 +9,21 @@ namespace LibraryCore.Tests.Core.GlobalMocks
 {
     public class FullMockIDistributedCache : IDistributedCache
     {
-        private Dictionary<string, byte[]> CacheStore { get; } = new Dictionary<string, byte[]>();
+        private ConcurrentDictionary<string, byte[]> CacheStore { get; } = new();
 
-        public byte[] Get(string key) => CacheStore.TryGetValue(key, out var tryGet) ? tryGet : default;
+        public byte[] Get(string key) => CacheStore.GetValueOrDefault(key);
 
-        public Task<byte[]> GetAsync(string key, CancellationToken token = default) => CacheStore.TryGetValue(key, out var tryGet) ? Task.FromResult(tryGet) : Task.FromResult<byte[]>(default);
+        public Task<byte[]> GetAsync(string key, CancellationToken token = default) => Task.FromResult(CacheStore.GetValueOrDefault(key));
 
         public void Refresh(string key) => throw new NotImplementedException();
 
         public Task RefreshAsync(string key, CancellationToken token = default) => throw new NotImplementedException();
 
-        public void Remove(string key) => CacheStore.Remove(key);
+        public void Remove(string key) => CacheStore.TryRemove(key, out _);
 
-        public Task RemoveAsync(string key, CancellationToken token = default) => Task.FromResult(CacheStore.Remove(key));
+        public Task RemoveAsync(string key, CancellationToken token = default) => Task.FromResult(CacheStore.TryRemove(key, out _));
 
-        public void Set(string key, byte[] value, DistributedCacheEntryOptions options) => CacheStore[key] = value;
+        public void Set(string key, byte[] value, DistributedCacheEntryOptions options) => CacheStore.TryAdd(key, value);
 
         public Task SetAsync(string key, byte[] value, DistributedCacheEntryOptions options, CancellationToken token = default)
         {
