69a8a01c6ac234970e22e3314f957f0acaa59547
Jason <dibiancoj@gmail.com>
2021-11-26 15:28:57 -0500

    add a command line parser for the performance tests

---
+++ PerformanceTests/LibraryCore.Performance.Tests/TestHarnessProvider/IPerformanceTest.cs
@@ -0,0 +1,8 @@
+﻿namespace LibraryCore.Performance.Tests.TestHarnessProvider
+{
+    public interface IPerformanceTest
+    {
+        public string CommandName { get; }
+        public string Description { get; }
+    }
+}
---
+++ PerformanceTests/LibraryCore.Performance.Tests/Tests/TemplatePerfTest.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+using LibraryCore.Performance.Tests.TestHarnessProvider;
+using System.Text;
+
+namespace LibraryCore.Performance.Tests.Tests
+{
+    [SimpleJob]
+    [MemoryDiagnoser]
+    public class TemplatePerfTest : IPerformanceTest
+    {
+        public string CommandName => "Template";
+        public string Description => "Run performance test on the base template";
+
+        [GlobalSetup]
+        public void Init()
+        {
+        }
+
+        [Benchmark(Baseline = true)]
+        public string CurrentImplementation()
+        {
+            return new StringBuilder()
+               .Append($"Item {1}")
+               .ToString();
+        }
+
+        [Benchmark]
+        public string StringBuilderImplementation()
+        {
+            return new StringBuilder()
+                .Append("Item 1")
+                .ToString();
+        }
+
+    }
+}
