9b85bd82c550d0ed636955615af97c610ff3dac1
Jason <dibiancoj@gmail.com>
2021-12-20 16:37:14 -0500

    render service integration tests

---
+++ IntegrationTests/Framework/LibraryCore.IntegrationTests.Framework.Api/Controllers/RenderServiceController.cs
@@ -0,0 +1,26 @@
+﻿using LibraryCore.AspNet.Render;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryCore.IntegrationTests.Framework.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RenderServiceController : Controller
+    {
+        public RenderServiceController(IRenderService renderService)
+        {
+            RenderService = renderService;
+        }
+
+        private IRenderService RenderService { get; }
+
+        [HttpGet("CantFindView")]
+        public async Task<IActionResult> CantFindView() => Json(await RenderService.RenderToStringAsync("~/Views/RenderService/CantFindView.cshtml"));
+
+        [HttpGet("WithModel")]
+        public async Task<IActionResult> WithModel() => Json(await RenderService.RenderToStringAsync("~/Views/RenderService/WithModel.cshtml", new KeyValuePair<string, string>("key1", "value1")));
+
+        [HttpGet("NoModel")]
+        public async Task<IActionResult> NoModel() => Json(await RenderService.RenderToStringAsync("~/Views/RenderService/NoModel.cshtml"));
+    }
+}
---
+++ IntegrationTests/LibraryCore.IntegrationTests/RenderServiceIntegrationTest.cs
@@ -0,0 +1,42 @@
+﻿using LibraryCore.IntegrationTests.Fixtures;
+
+namespace LibraryCore.IntegrationTests
+{
+    public class RenderServiceIntegrationTest : IClassFixture<WebApplicationFactoryFixture>
+    {
+        public RenderServiceIntegrationTest(WebApplicationFactoryFixture webApplicationFactoryFixture)
+        {
+            WebApplicationFactoryFixture = webApplicationFactoryFixture;
+        }
+
+        private WebApplicationFactoryFixture WebApplicationFactoryFixture { get; }
+
+        [Fact]
+        public async Task CantFindView()
+        {
+            var response = await WebApplicationFactoryFixture.HttpClientToUse.GetAsync("RenderService/CantFindView");
+
+            Assert.False(response.IsSuccessStatusCode);
+        }
+
+        [Fact]
+        public async Task RenderServiceWithNoModel()
+        {
+            var response = await WebApplicationFactoryFixture.HttpClientToUse.GetAsync("RenderService/NoModel");
+
+            var result = await response.EnsureSuccessStatusCode().Content.ReadAsStringAsync();
+
+            Assert.Equal("\"\\u003Cdiv\\u003ETest\\u003C/div\\u003E\"", result);
+        }
+
+        [Fact]
+        public async Task RenderServiceWithModel()
+        {
+            var response = await WebApplicationFactoryFixture.HttpClientToUse.GetAsync("RenderService/WithModel");
+
+            var result = await response.EnsureSuccessStatusCode().Content.ReadAsStringAsync();
+
+            Assert.Equal("\"\\r\\n\\u003Cdiv aria-key=key1 aria-value=value1\\u003E\\r\\n\\u003C/div\\u003E\"", result);
+        }
+    }
+}
