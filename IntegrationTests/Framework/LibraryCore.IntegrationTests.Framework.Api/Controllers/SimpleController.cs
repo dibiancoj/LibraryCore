88bfe286aebc449f1c27dde3dcf22931489c3de4
Jason <dibiancoj@gmail.com>
2021-12-21 10:12:47 -0500

    validation

--- IntegrationTests/Framework/LibraryCore.IntegrationTests.Framework.Api/Controllers/SimpleController.cs
+++ IntegrationTests/Framework/LibraryCore.IntegrationTests.Framework.Api/Controllers/SimpleController.cs
@@ -1,4 +1,5 @@
 ﻿using LibraryCore.AspNet.Attributes;
+using LibraryCore.AspNet.Validation;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryCore.IntegrationTests.Framework.Api.Controllers
@@ -9,5 +10,14 @@ namespace LibraryCore.IntegrationTests.Framework.Api.Controllers
     {
         [HttpGetOptionHead("AspNetHttpMethodTest")]
         public bool AspNetHttpMethodTest() => true;
+
+        [HttpPost("ValidationTest")]
+        public bool ValidationTest([FromBody] ValidationTest model) => true;
+    }
+
+    public class ValidationTest
+    {
+        [DateOfBirthRange]
+        public DateTime? DataOfBirth { get; set; }
     }
 }
---
+++ IntegrationTests/LibraryCore.IntegrationTests/AspNetValidationIntegrationTest.cs
@@ -0,0 +1,32 @@
+﻿using LibraryCore.IntegrationTests.Fixtures;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace LibraryCore.IntegrationTests
+{
+    public class AspNetValidationIntegrationTest : IClassFixture<WebApplicationFactoryFixture>
+    {
+        public AspNetValidationIntegrationTest(WebApplicationFactoryFixture webApplicationFactoryFixture)
+        {
+            WebApplicationFactoryFixture = webApplicationFactoryFixture;
+        }
+
+        private WebApplicationFactoryFixture WebApplicationFactoryFixture { get; }
+
+        [InlineData(true, -2)]
+        [InlineData(false, -500)]
+        [Theory]
+        public async Task DateOfBirthTestIsValid(bool expectedToBeSuccessful, int howManyYearsAgoWereTheyBorn)
+        {
+            var response = await WebApplicationFactoryFixture.HttpClientToUse.PostAsJsonAsync("Simple/ValidationTest", new ValidationTest { DataOfBirth = DateTime.Now.AddYears(howManyYearsAgoWereTheyBorn) });
+
+            Assert.Equal(expectedToBeSuccessful, response.IsSuccessStatusCode);
+            Assert.Equal(expectedToBeSuccessful ? HttpStatusCode.OK : HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+
+    public class ValidationTest
+    {
+        public DateTime? DataOfBirth { get; set; }
+    }
+}
