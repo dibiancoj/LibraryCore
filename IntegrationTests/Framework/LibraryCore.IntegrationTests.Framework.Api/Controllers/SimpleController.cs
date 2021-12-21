d248236bea08ad70ba1edee7c910a65cf2d7bf0d
Jason <dibiancoj@gmail.com>
2021-12-21 09:45:21 -0500

    http get or options integration test

---
+++ IntegrationTests/Framework/LibraryCore.IntegrationTests.Framework.Api/Controllers/SimpleController.cs
@@ -0,0 +1,13 @@
+﻿using LibraryCore.AspNet.Attributes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryCore.IntegrationTests.Framework.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SimpleController : Controller
+    {
+        [HttpGetOptionHead("AspNetHttpMethodTest")]
+        public bool AspNetHttpMethodTest() => true;
+    }
+}
