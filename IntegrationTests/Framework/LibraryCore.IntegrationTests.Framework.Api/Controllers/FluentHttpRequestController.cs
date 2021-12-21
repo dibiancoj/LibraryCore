f0d5920c0689f040f10756ec5bb9c60372a2871a
Jason DiBianco <dibiancoj@gmail.com>
2021-12-20 19:24:53 -0500

    namespace cleanup (#111)

--- IntegrationTests/Framework/LibraryCore.IntegrationTests.Framework.Api/Controllers/FluentHttpRequestController.cs
+++ IntegrationTests/Framework/LibraryCore.IntegrationTests.Framework.Api/Controllers/FluentHttpRequestController.cs
@@ -1,42 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace LibraryCore.IntegrationTests.Framework.Api.Controllers
+namespace LibraryCore.IntegrationTests.Framework.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FluentHttpRequestController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class FluentHttpRequestController : ControllerBase
+    #region Models
+
+    public record ResultModel(int Id, string Text);
+
+    public class XmlRoot
     {
-
-        #region Models
-        public record ResultModel(int Id, string Text);
-
-        public class XmlRoot
-        {
-            public int Id { get; set; }
-        }
-
-        #endregion
-
-        [HttpGet("HeaderTest")]
-        public string HeaderTest() => $"{Request.Headers["MyHeader"].First()} Result";
-
-        [HttpGet("QueryStringTest")]
-        public string QueryStringTest() => $"{Request.Query["Q1"]}:{Request.Query["Q2"]}";
-
-        [HttpGet("SimpleJsonPayload")]
-        public ResultModel SimpleJsonPayload() => new(9999, "1111");
-
-        [HttpGet("SimpleXmlPayload")]
-        public XmlRoot SimpleXmlPayload() => new() { Id = 5 };
-
-        [HttpGet("SimpleJsonPayloadWithJsonParameters")]
-        public ResultModel SimpleJsonPayloadWithJsonParameters([FromBody] ResultModel parameters) => new(parameters.Id + 1, parameters.Text + "_Result");
-
-        [Consumes("application/x-www-form-urlencoded")]
-        [HttpGet("FormsEncodedParameters")]
-        public IEnumerable<ResultModel> FormsEncodedParameters([FromForm] IFormCollection parameters) => parameters.Select(t => new ResultModel(Convert.ToInt32(t.Key) + 10, t.Value + "_Result"));
-
-        [HttpPost("FileUploadStream")]
-        public IEnumerable<ResultModel> FileUploadStream(IEnumerable<IFormFile> formFiles) => formFiles.Select(file => new ResultModel(Convert.ToInt32(file.Length), file.FileName));
+        public int Id { get; set; }
     }
+
+    #endregion
+
+    [HttpGet("HeaderTest")]
+    public string HeaderTest() => $"{Request.Headers["MyHeader"].First()} Result";
+
+    [HttpGet("QueryStringTest")]
+    public string QueryStringTest() => $"{Request.Query["Q1"]}:{Request.Query["Q2"]}";
+
+    [HttpGet("SimpleJsonPayload")]
+    public ResultModel SimpleJsonPayload() => new(9999, "1111");
+
+    [HttpGet("SimpleXmlPayload")]
+    public XmlRoot SimpleXmlPayload() => new() { Id = 5 };
+
+    [HttpGet("SimpleJsonPayloadWithJsonParameters")]
+    public ResultModel SimpleJsonPayloadWithJsonParameters([FromBody] ResultModel parameters) => new(parameters.Id + 1, parameters.Text + "_Result");
+
+    [Consumes("application/x-www-form-urlencoded")]
+    [HttpGet("FormsEncodedParameters")]
+    public IEnumerable<ResultModel> FormsEncodedParameters([FromForm] IFormCollection parameters) => parameters.Select(t => new ResultModel(Convert.ToInt32(t.Key) + 10, t.Value + "_Result"));
+
+    [HttpPost("FileUploadStream")]
+    public IEnumerable<ResultModel> FileUploadStream(IEnumerable<IFormFile> formFiles) => formFiles.Select(file => new ResultModel(Convert.ToInt32(file.Length), file.FileName));
 }
--- IntegrationTests/LibraryCore.IntegrationTests/Fixtures/WebApplicationFactoryFixture.cs
+++ IntegrationTests/LibraryCore.IntegrationTests/Fixtures/WebApplicationFactoryFixture.cs
@@ -1,56 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
 
-namespace LibraryCore.IntegrationTests.Fixtures
+namespace LibraryCore.IntegrationTests.Fixtures;
+
+/// <summary>
+/// This is an xunit fixture to contain a singleton for all integration tests.
+/// </summary>
+public class WebApplicationFactoryFixture : IDisposable
 {
-    /// <summary>
-    /// This is an xunit fixture to contain a singleton for all integration tests.
-    /// </summary>
-    public class WebApplicationFactoryFixture: IDisposable
+    public WebApplicationFactoryFixture()
     {
-        public WebApplicationFactoryFixture()
-        {
-            ApplicationFactory = new WebApplicationFactory<Program>()
-            .WithWebHostBuilder(builder =>
-              {
+        ApplicationFactory = new WebApplicationFactory<Program>()
+        .WithWebHostBuilder(builder =>
+          {
                   // ... Configure test services
               });
 
-            HttpClientToUse = ApplicationFactory.CreateClient();
-        }
-
-        public WebApplicationFactory<Program> ApplicationFactory { get; }
-        public HttpClient HttpClientToUse { get; }
-        private bool Disposed { get; set; }
-
-        #region Dispose Method
-
-        /// <summary>
-        /// Disposes My Object
-        /// </summary>
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        /// <summary>
-        /// Dispose Overload. Ensures my database connection is closed
-        /// </summary>
-        private void Dispose(bool disposing)
-        {
-            if (!this.Disposed)
-            {
-                if (disposing)
-                {
-                    ApplicationFactory.Dispose();
-                    HttpClientToUse.Dispose();
-
-                }
-            }
-            Disposed = true;
-        }
-
-        #endregion
-
+        HttpClientToUse = ApplicationFactory.CreateClient();
     }
+
+    public WebApplicationFactory<Program> ApplicationFactory { get; }
+    public HttpClient HttpClientToUse { get; }
+    private bool Disposed { get; set; }
+
+    #region Dispose Method
+
+    /// <summary>
+    /// Disposes My Object
+    /// </summary>
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    /// <summary>
+    /// Dispose Overload. Ensures my database connection is closed
+    /// </summary>
+    private void Dispose(bool disposing)
+    {
+        if (!this.Disposed)
+        {
+            if (disposing)
+            {
+                ApplicationFactory.Dispose();
+                HttpClientToUse.Dispose();
+
+            }
+        }
+        Disposed = true;
+    }
+
+    #endregion
+
 }
