f0adc69600ffedd90275a0855587cade3812d9de
Jason <dibiancoj@gmail.com>
2022-02-03 21:30:38 -0500

    double peak vs func invoke

---
+++ PerformanceTests/LibraryCore.Performance.Tests/Tests/StringReaderFuncVsDoublePeekPerfTest.cs
@@ -0,0 +1,50 @@
+﻿using BenchmarkDotNet.Attributes;
+using LibraryCore.Performance.Tests.TestHarnessProvider;
+
+namespace LibraryCore.Performance.Tests.Tests
+{
+    [SimpleJob]
+    [MemoryDiagnoser]
+    public class StringReaderFuncVsDoublePeekPerfTest : IPerformanceTest
+    {
+        public string CommandName => "StringReaderFuncVs2Peek";
+        public string Description => "Run performance test on invoking a func vs calling peek twice";
+
+        [Benchmark(Baseline = true)]
+        public int DoublePeek()
+        {
+            using var reader = new StringReader("abcdef");
+            int c = 0;
+
+            while (reader.Peek() != -1 && reader.Peek() != 'z')
+            {
+                c = reader.Read();
+            }
+
+            return c;
+        }
+
+        [Benchmark]
+        public int FuncInvoke()
+        {
+            using var reader = new StringReader("abcdef");
+            int c = 0;
+
+            while (Helper(reader, x => x != 'z'))
+            {
+                c = reader.Read();
+            }
+
+            return c;
+        }
+
+        private static bool Helper(StringReader reader, Func<int, bool> tester)
+        {
+            var peek = reader.Peek();
+
+            return peek != -1 && tester(peek);
+        }
+
+    }
+
+}
