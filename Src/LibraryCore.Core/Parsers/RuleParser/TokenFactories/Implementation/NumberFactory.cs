065db8631a3e4c5785c62505aca238cd9fba4292
Jason Dibianco <dibiancj@mskcc.org>
2022-02-08 09:50:53 -0500

    cleanup

--- Src/LibraryCore.Core/Parsers/RuleParser/TokenFactories/Implementation/NumberFactory.cs
+++ Src/LibraryCore.Core/Parsers/RuleParser/TokenFactories/Implementation/NumberFactory.cs
@@ -11,13 +11,6 @@ public class NumberFactory : ITokenFactory
 
     public IToken CreateToken(char characterRead, StringReader stringReader, TokenFactoryProvider tokenFactoryProvider)
     {
-        static bool IsFinalCharacter(StringReader readerToUse)
-        {
-            var peekedCharacter = readerToUse.PeekCharacter();
-
-            return !char.IsWhiteSpace(peekedCharacter) && peekedCharacter != 'd' && peekedCharacter != '?';
-        }
-
         var text = new StringBuilder().Append(characterRead);
 
         while (stringReader.HasMoreCharacters() && IsFinalCharacter(stringReader))
@@ -25,34 +18,90 @@ public class NumberFactory : ITokenFactory
             text.Append(stringReader.ReadCharacter());
         }
 
-        var peekNextCharacter = stringReader.PeekCharacter();
+        //we need to handle if this is nullable or a double (double = 'd', nullable = '?')
+        var (IsDouble, IsNullable) = WalkAdditionalCharacters(stringReader);
 
-        return peekNextCharacter == 'D' || peekNextCharacter == 'd' ?
-            CreateDoubleToken(stringReader, text):
-            CreateIntToken(stringReader, text);
+        var typeToUse = DetermineType(IsDouble, IsNullable);
+
+        return IsDouble ?
+            CreateDoubleToken(typeToUse, text) :
+            CreateIntToken(typeToUse, text);
     }
 
-    private static IToken CreateDoubleToken(StringReader stringReader, StringBuilder textFound)
+    private static Type DetermineType(bool isDouble, bool isNullable)
     {
-        //remove the last character which is d
-        RuleParsingUtility.ThrowIfCharacterNotExpected(stringReader, 'd');
+        if (isDouble && isNullable)
+        {
+            return typeof(double?);
+        }
 
+        if (isDouble)
+        {
+            return typeof(double);
+        }
+
+        if (isNullable)
+        {
+            return typeof(int?);
+        }
+
+        return typeof(int);
+    }
+
+    private static bool IsFinalCharacter(StringReader readerToUse)
+    {
+        var peekedCharacter = readerToUse.PeekCharacter();
+
+        return !char.IsWhiteSpace(peekedCharacter) && peekedCharacter != 'd' && peekedCharacter != '?';
+    }
+
+    private static (bool IsDouble, bool IsNullable) WalkAdditionalCharacters(StringReader stringReader)
+    {
+        var peekNextCharacter = stringReader.PeekCharacter();
+
+        if (peekNextCharacter != 'd' && peekNextCharacter != '?')
+        {
+            return (false, false);
+        }
+
+        bool isDouble = false;
+        bool isNullable = false;
+
+        while (stringReader.HasMoreCharacters() && !char.IsWhiteSpace(stringReader.PeekCharacter()))
+        {
+            var readCharacter = stringReader.ReadCharacter();
+
+            if (readCharacter == 'd')
+            {
+                isDouble = true;
+            }
+            else if (readCharacter == '?')
+            {
+                isNullable = true;
+            }
+        }
+
+        return (isDouble, isNullable);
+    }
+
+    private static IToken CreateDoubleToken(Type typeToUse, StringBuilder textFound)
+    {
         if (!double.TryParse(textFound.ToString(), out double number))
         {
             throw new Exception("Number Factory Not Able To Parse Number. Value = " + textFound.ToString());
         }
 
-        return new NumberDoubleToken(number, RuleParsingUtility.DetermineNullableType<double, double?>(stringReader));
+        return new NumberDoubleToken(number, typeToUse);
     }
 
-    private static IToken CreateIntToken(StringReader stringReader, StringBuilder textFound)
+    private static IToken CreateIntToken(Type typeToUse, StringBuilder textFound)
     {
         if (!int.TryParse(textFound.ToString(), out int number))
         {
             throw new Exception("Number Factory Not Able To Parse Number. Value = " + textFound);
         }
 
-        return new NumberToken(number, RuleParsingUtility.DetermineNullableType<int, int?>(stringReader));
+        return new NumberToken(number, typeToUse);
     }
 }
 
