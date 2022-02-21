bfee374a8e8acef0108b53a78a90119c793bd1db
Jason <dibiancoj@gmail.com>
2022-02-21 11:11:11 -0500

    string span reader logic.

---
+++ Src/LibraryCore.Core/Readers/StringSpanReader.cs
@@ -0,0 +1,51 @@
+﻿namespace LibraryCore.Core.Readers;
+
+/// <summary>
+/// This a strict ensure you pass a ref value into any methods otherwise, the Index get's copied resulting in weird issues
+/// </summary>
+public ref struct StringSpanReader
+{
+    public StringSpanReader(string stringToParse)
+    {
+        StringToParse = stringToParse;
+        Index = 0;
+    }
+
+    private ReadOnlySpan<char> StringToParse { get; }
+    private int Index { get; set; }
+
+    public bool HasMoreCharacters() => Index < StringToParse.Length;
+    public char? Peek() => HasMoreCharacters() ? StringToParse[Index] : null;
+    public char? Read() => HasMoreCharacters() ? StringToParse[Index++] : null;
+
+    public string? Peek(int numberOfCharacters)
+    {
+        if (!HasMoreCharacters())
+        {
+            return null;
+        }
+
+        return new string(DontHaveEnoughCharactersLeft(numberOfCharacters) ?
+                                    StringToParse[Index..] :
+                                    StringToParse.Slice(Index, numberOfCharacters));
+    }
+
+    public string? Read(int numberOfCharacters)
+    {
+        if (!HasMoreCharacters())
+        {
+            return null;
+        }
+
+        var stringToReturn = new string(DontHaveEnoughCharactersLeft(numberOfCharacters) ?
+                                    StringToParse[Index..] :
+                                    StringToParse.Slice(Index, numberOfCharacters));
+
+        //need to fast forward the index. This way its sitting at the character we are up to after the read x amount of characters
+        Index += numberOfCharacters;
+
+        return stringToReturn;
+    }
+
+    private bool DontHaveEnoughCharactersLeft(int numberOfCharactersToScan) => (Index + numberOfCharactersToScan) > StringToParse.Length;
+}
---
+++ Tests/LibraryCore.Tests.Core/Readers/StringSpanReaderTest.cs
@@ -0,0 +1,89 @@
+﻿using LibraryCore.Core.Readers;
+
+namespace LibraryCore.Tests.Core.Readers;
+
+public class StringSpanReaderTest
+{
+    [Fact]
+    public void ReadCorrectly()
+    {
+        var reader = new StringSpanReader("Abc");
+
+        Assert.Equal('A', reader.Read());
+        Assert.Equal('b', reader.Read());
+        Assert.Equal('c', reader.Read());
+        Assert.Null(reader.Read());
+    }
+
+    [Fact]
+    public void HasMoreCharacters()
+    {
+        var reader = new StringSpanReader("bc");
+
+        Assert.True(reader.HasMoreCharacters());
+        reader.Read(); //should be at c
+        Assert.True(reader.HasMoreCharacters());
+        reader.Read(); //should be after c
+        Assert.False(reader.HasMoreCharacters());
+    }
+
+    [Fact]
+    public void PeekCorrectly()
+    {
+        var reader = new StringSpanReader("Abc");
+
+        Assert.Equal('A', reader.Peek());
+        Assert.Equal('A', reader.Read());
+        Assert.Equal('b', reader.Peek());
+        Assert.Equal('b', reader.Read());
+        Assert.Equal('c', reader.Peek());
+        Assert.Equal('c', reader.Read());
+        Assert.Null(reader.Peek());
+    }
+
+    [Fact]
+    public void PeekMultipleWhenNotEnoughCharacters() => Assert.Equal("Abc", new StringSpanReader("Abc").Peek(5));
+
+    [Fact]
+    public void PeekMultipleWhenAtTheEnd() => Assert.Null(new StringSpanReader("").Peek(1));
+
+    [Fact]
+    public void PeekMultipleWhenHaveCharactersLeftOverAfterPeek() => Assert.Equal("Ab", new StringSpanReader("Abcdef").Peek(2));
+
+    [Fact]
+    public void ReadMultipleWhenNotEnoughCharacters() => Assert.Equal("Abc", new StringSpanReader("Abc").Read(5));
+
+    [Fact]
+    public void ReadMultipleWhenAtTheEnd() => Assert.Null(new StringSpanReader("").Read(1));
+
+    [Fact]
+    public void ReadMultipleWhenHaveCharactersLeftOverAfterPeek()
+    {
+        var reader = new StringSpanReader("Abcdef");
+
+        Assert.Equal("Ab", reader.Read(2));
+
+        //now make sure we are up to c
+        Assert.Equal('c', reader.Peek());
+    }
+
+    [Fact]
+    public void EnsurePassingStructToMethodsWork()
+    {
+        var reader = new StringSpanReader("Abcdef");
+
+        Assert.Equal('A', reader.Read());
+
+        PassStruct1(ref reader);
+        Assert.Equal('d', reader.Read());
+    }
+
+    private static void PassStruct1(ref StringSpanReader reader)
+    {
+        Assert.Equal('b', reader.Read());
+        PassStruct2(ref reader);
+    }
+
+    private static void PassStruct2(ref StringSpanReader reader) => Assert.Equal('c', reader.Read());
+
+}
