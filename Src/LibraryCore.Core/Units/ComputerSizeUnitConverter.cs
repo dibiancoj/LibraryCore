ed8797d50fe49964648e292807f3b478caa0083a
Jason <dibiancoj@gmail.com>
2022-03-05 13:58:50 -0500

    fix case of local variable

--- Src/LibraryCore.Core/Units/ComputerSizeUnitConverter.cs
+++ Src/LibraryCore.Core/Units/ComputerSizeUnitConverter.cs
@@ -63,7 +63,7 @@ public static class ComputerSizeUnitConverter
         //how many times we need to loop through and keep adding to the figure
 
         //how many levels ^ computer base...so every level we either * by 1024...or / by 1024. (start with 1 if we are going from the same from and to level)
-        double LevelsPerComputerBase = 1;
+        double levelsPerComputerBase = 1;
 
         //are we going to different levels?
         if (fromUnit != toUnit)
@@ -71,18 +71,18 @@ public static class ComputerSizeUnitConverter
             //we are going multiple levels, so we need to calculate how many levels we are going (we can go higher or lower)
 
             //so we get the # of units we are going. Then grab the power...so we are going 2 levels and then do (1024 ^ 2 levels)
-            LevelsPerComputerBase = Math.Pow(computerBase, Math.Abs(fromUnit - toUnit));
+            levelsPerComputerBase = Math.Pow(computerBase, Math.Abs(fromUnit - toUnit));
         }
 
         //if we are going smaller to lower then we need to divide the LevelsPerComputerBase by / 
         if (fromUnit < toUnit)
         {
             //we need to divide by 1024 to get to the smaller unit
-            return valueToConvert / LevelsPerComputerBase;
+            return valueToConvert / levelsPerComputerBase;
         }
 
         //let's multiple the value we want to convert to the working figure...(at this point we have the multipler from the base unit to the ToUnit)
-        return valueToConvert * LevelsPerComputerBase;
+        return valueToConvert * levelsPerComputerBase;
     }
 
     #endregion
