12f29931624963fafe5bdbf2833667084fb55024
Jason DiBianco <dibiancoj@gmail.com>
2022-04-09 14:01:31 -0400

    Adjust the batch command for performance tests. This will consumer the test name and run the test directly using 'commands/RunPerformanceTest.bat' (#152)

--- Commands/RunPerformanceTest.bat
+++ Commands/RunPerformanceTest.bat
@@ -1,4 +1,5 @@
 cd..
 
+set /p test="Enter the test name to run: "
 dotnet build --configuration release
-dotnet run --configuration release --project PerformanceTests\LibraryCore.Performance.Tests
+dotnet run --configuration release --project PerformanceTests\LibraryCore.Performance.Tests %test%
