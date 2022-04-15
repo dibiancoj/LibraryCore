f1c64daeef39281b2c0b52ca89dd8836577cb814
Jason <dibiancoj@gmail.com>
2022-04-14 22:21:04 -0400

    fix the bad merge request

---
+++ Tests/LibraryCore.Tests.Core/DataTypes/TryToGetValueResultTest.cs
@@ -0,0 +1,30 @@
+﻿using LibraryCore.Core.DataTypes;
+
+namespace LibraryCore.Tests.Core.DataTypes;
+
+public class TryToGetValueResultTest
+{
+    [Fact]
+    public void NotFound()
+    {
+        var result = new TryToGetValueResult<string>(false, null);
+
+        Assert.False(result.ItemWasFound);
+        Assert.Null(result.ItemValueIfFound);
+
+        Assert.False(result.GetValueIfFound(out var itemIfFound));
+        Assert.Null(itemIfFound);
+    }
+
+    [Fact]
+    public void WasFound()
+    {
+        var result = new TryToGetValueResult<string>(true, "test123");
+
+        Assert.True(result.ItemWasFound);
+        Assert.Equal("test123", result.ItemValueIfFound);
+
+        Assert.True(result.GetValueIfFound(out var itemIfFound));
+        Assert.Equal("test123", itemIfFound);
+    }
+}
