3ff3b87edb511cf1f9327d64f18b87911ed4b012
Jason DiBianco <dibiancoj@gmail.com>
2022-06-15 09:33:13 -0400

    Delimiter - Create a more flexible parser and builder (#170)
    * Redo our delimiter builder to handle more scenarios.
    
    * cleanup
    
    * Update the package version for core.
    
    * cleanup

--- Tests/LibraryCore.Tests.Core/Delimiter/DelimiterReaderTest.cs
+++ Tests/LibraryCore.Tests.Core/Delimiter/DelimiterReaderTest.cs
@@ -7,32 +7,56 @@ public class DelimiterReaderTest
     [Fact]
     public void ParseFromLinesLazyTest()
     {
-        var result = DelimiterReader.ParseFromLinesLazy(new[] { "abc|cde|fgh", "111|222|333" }, "|").ToArray();
+        var result = DelimiterReader.ParseFromLinesLazy(new[] { @"""abc""|""cde""|""fgh""", @"""111""|""222""|""333""" }, '|').ToArray();
 
         Assert.Equal(2, result.Length);
 
-        Assert.Equal("abc", result[0].ColumnData[0]);
-        Assert.Equal("cde", result[0].ColumnData[1]);
-        Assert.Equal("fgh", result[0].ColumnData[2]);
+        Assert.Equal("abc", result[0].Value<string>(0));
+        Assert.Equal("cde", result[0].Value<string>(1));
+        Assert.Equal("fgh", result[0].Value<string>(2));
 
-        Assert.Equal("111", result[1].ColumnData[0]);
-        Assert.Equal("222", result[1].ColumnData[1]);
-        Assert.Equal("333", result[1].ColumnData[2]);
+        Assert.Equal(111, result[1].Value<int>(0));
+        Assert.Equal(222, result[1].Value<int>(1));
+        Assert.Equal(333, result[1].Value<int>(2));
     }
 
     [Fact]
     public void ParseFromTextLinesLazyTest()
     {
-        var result = DelimiterReader.ParseFromTextLinesLazy("abc|cde|fgh" + Environment.NewLine + "111|222|333", "|").ToArray();
+        var result = DelimiterReader.ParseFromTextLinesLazy(@"""abc""|""cde""|""fgh""" + Environment.NewLine + @"""111""|""222""|""333""", '|').ToArray();
 
         Assert.Equal(2, result.Length);
 
-        Assert.Equal("abc", result[0].ColumnData[0]);
-        Assert.Equal("cde", result[0].ColumnData[1]);
-        Assert.Equal("fgh", result[0].ColumnData[2]);
+        Assert.Equal("abc", result[0].Value<string>(0));
+        Assert.Equal("cde", result[0].Value<string>(1));
+        Assert.Equal("fgh", result[0].Value<string>(2));
 
-        Assert.Equal("111", result[1].ColumnData[0]);
-        Assert.Equal("222", result[1].ColumnData[1]);
-        Assert.Equal("333", result[1].ColumnData[2]);
+        Assert.Equal(111, result[1].Value<int>(0));
+        Assert.Equal(222, result[1].Value<int>(1));
+        Assert.Equal(333, result[1].Value<int>(2));
+    }
+
+    [Fact]
+    public void ParseWithDelimiterInString()
+    {
+        var result = DelimiterReader.ParseFromTextLinesLazy(@"""a|b|c""|""cde""|""fgh""", '|').ToArray();
+
+        Assert.Single(result);
+
+        Assert.Equal("a|b|c", result[0].Value<string>(0));
+        Assert.Equal("cde", result[0].Value<string>(1));
+        Assert.Equal("fgh", result[0].Value<string>(2));
+    }
+
+    [Fact]
+    public void ParseWithQuoteInString()
+    {
+        var result = DelimiterReader.ParseFromTextLinesLazy(@"""ab""""c""|""def""|""zzz""", '|').ToArray();
+
+        Assert.Single(result);
+
+        Assert.Equal(@"ab""c", result[0].Value<string>(0));
+        Assert.Equal("def", result[0].Value<string>(1));
+        Assert.Equal("zzz", result[0].Value<string>(2));
     }
 }
