deb90f59ee0d2231f69da7928f59b06f70cbeba5
Jason <dibiancoj@gmail.com>
2022-07-17 22:06:01 -0400

    Performance metrics for system.text.json for serialization and deserialization to string or byte array.

---
+++ PerformanceTests/LibraryCore.Performance.Tests/PerfTests/Tests/JsonDeSerializerByteVsJson.cs
@@ -0,0 +1,51 @@
+﻿using BenchmarkDotNet.Attributes;
+using LibraryCore.Performance.Tests.TestHarnessProvider;
+using System.Text.Json;
+using static LibraryCore.Performance.Tests.Program;
+
+namespace LibraryCore.Performance.Tests.PerfTests;
+
+[SimpleJob]
+[Config(typeof(Config))]
+[MemoryDiagnoser]
+public class JsonDeSerializerByteVsJson : IPerformanceTest
+{
+    public string CommandName => "JsonDeSerializerByteVsJson";
+    public string Description => "Does System.Text.Json deserialize a byte array faster then a string";
+
+    [GlobalSetup]
+    public void Init()
+    {
+        var myModel = new MyRecord { Id = 99999, Text = "My Really Long Text Value Text Value", SecondaryId = Guid.NewGuid(), IsActive = true, CreatedDate = DateTime.Now, Ids = Enumerable.Range(0, 10).Select(x => x).ToList() };
+        ByteArrayData = JsonSerializer.SerializeToUtf8Bytes(myModel);
+        StringArrayData = JsonSerializer.Serialize(myModel);
+        JsonSerializerOption = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+    }
+
+    private JsonSerializerOptions JsonSerializerOption { get; set; }
+    private byte[] ByteArrayData { get; set; }
+    private string StringArrayData { get; set; }
+
+    [Benchmark(Baseline = true)]
+    public MyRecord ByteArray()
+    {
+        return JsonSerializer.Deserialize<MyRecord>(ByteArrayData, JsonSerializerOption);
+    }
+
+    [Benchmark]
+    public MyRecord String()
+    {
+        return JsonSerializer.Deserialize<MyRecord>(StringArrayData, JsonSerializerOption);
+    }
+
+    public class MyRecord
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public Guid SecondaryId { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public IEnumerable<int> Ids { get; set; }
+    }
+
+}
---
+++ PerformanceTests/LibraryCore.Performance.Tests/PerfTests/Tests/JsonSerializerByteVsJson.cs
@@ -0,0 +1,48 @@
+﻿using BenchmarkDotNet.Attributes;
+using LibraryCore.Performance.Tests.TestHarnessProvider;
+using System.Text.Json;
+using static LibraryCore.Performance.Tests.Program;
+
+namespace LibraryCore.Performance.Tests.PerfTests;
+
+[SimpleJob]
+[Config(typeof(Config))]
+[MemoryDiagnoser]
+public class JsonSerializerByteVsJson : IPerformanceTest
+{
+    public string CommandName => "JsonSerializerByteVsJson";
+    public string Description => "Does System.Text.Json serialize a byte array faster then a string";
+
+    [GlobalSetup]
+    public void Init()
+    {
+        var myModel = new MyRecord { Id = 99999, Text = "My Really Long Text Value Text Value", SecondaryId = Guid.NewGuid(), IsActive = true, CreatedDate = DateTime.Now, Ids = Enumerable.Range(0, 10).Select(x => x).ToList() };
+        JsonSerializerOption = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+    }
+
+    private MyRecord Model { get; set; }
+    private JsonSerializerOptions JsonSerializerOption { get; set; }
+
+    [Benchmark(Baseline = true)]
+    public byte[] ByteArray()
+    {
+        return JsonSerializer.SerializeToUtf8Bytes(Model, JsonSerializerOption);
+    }
+
+    [Benchmark]
+    public string String()
+    {
+        return JsonSerializer.Serialize(Model, JsonSerializerOption);
+    }
+
+    public class MyRecord
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public Guid SecondaryId { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public IEnumerable<int> Ids { get; set; }
+    }
+
+}
