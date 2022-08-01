bbb26403c130aa11d5b828bf0b46663449b2d6f2
Jason <dibiancoj@gmail.com>
2022-08-01 12:52:53 -0400

    switch command for integration. Also run in a non detached mode so we can see if the test failed

--- Commands/IntegratedTestInVsEnvSetup.ps1
+++ Commands/IntegratedTestInVsEnvSetup.ps1
@@ -1,4 +1,4 @@
 cd..
-$env:Db_ConnectionString = 'Pass@word';docker-compose up -d
+$env:DB_PW = 'Pass@word';docker-compose up
 cd Commands
 Write-Output "Sql Is Running In Docker Detached Mode. Everything Is Running..."
