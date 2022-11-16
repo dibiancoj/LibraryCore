52d73538768e789ffe3f7cf671dec5ecc484d5ef
Jason Dibianco <dibiancj@mskcc.org>
2022-11-16 08:42:00 -0500

    token calls

--- Src/LibraryCore.Core/ExtensionMethods/HttpClientExtensionMethods.cs
+++ Src/LibraryCore.Core/ExtensionMethods/HttpClientExtensionMethods.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http.Json;
+using System.Text.Json.Serialization;
 
 namespace LibraryCore.Core.ExtensionMethods;
 
@@ -22,5 +23,32 @@ public static class HttpClientExtensionMethods
     private static Task<HttpResponseMessage> SendMessageHelper(HttpClient httpClient, HttpRequestMessage requestMessage, CancellationToken cancellationToken = default)
     {
         return httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
+    }
+
+    public static async Task<Token?> TokenAsync(this HttpClient httpClient, Uri tokenUri, string clientId, string clientSecret, string grantType = "client_credentials", string scope = "Read", CancellationToken cancellationToken = default)
+    {
+        var rawResponse = await httpClient.PostAsync(tokenUri.AbsoluteUri, new FormUrlEncodedContent(new KeyValuePair<string, string>[]
+        {
+            new("client_id", clientId),
+            new("client_secret", clientSecret),
+            new("grant_type", grantType),
+            new("scope", scope),
+        }), cancellationToken).ConfigureAwait(false);
+
+        return await rawResponse.EnsureSuccessStatusCode().Content.ReadFromJsonAsync<Token>(cancellationToken: cancellationToken).ConfigureAwait(false);
+    }
+
+    /// <summary>
+    /// Client Credentials Response
+    /// </summary>
+    /// <param name="ExpiresIn">The remaining time in seconds that the consent remains valid</param>
+    /// <param name="ConsentedOn">The time in seconds since the epoch when the resource owner gave consent to the client</param>
+    public record Token([property: JsonPropertyName("token_type")] string TokenType,
+                        [property: JsonPropertyName("access_token")] string AccessToken,
+                        [property: JsonPropertyName("scope")] string Scope,
+                        [property: JsonPropertyName("expires_in")] int ExpiresIn,
+                        [property: JsonPropertyName("consented_on")] long ConsentedOn)
+    {
+        public DateTimeOffset ExpiresLocalTime { get; } = DateTimeOffset.FromUnixTimeSeconds(ConsentedOn).ToLocalTime().AddSeconds(ExpiresIn);
     }
 }
