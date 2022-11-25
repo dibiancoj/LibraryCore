6e30c385bb31f17941682e4f7d89910b5bfe7cba
Jason <dibiancoj@gmail.com>
2022-11-25 13:07:53 -0500

    remove warning.

--- IntegrationTests/Framework/LibraryCore.IntegrationTests.Framework.Api/Controllers/FluentHttpRequestController.cs
+++ IntegrationTests/Framework/LibraryCore.IntegrationTests.Framework.Api/Controllers/FluentHttpRequestController.cs
@@ -8,7 +8,14 @@ public class FluentHttpRequestController : ControllerBase
 {
     #region Models
 
-    public record ResultModel(int Id, string Text);
+    /// <summary>
+    /// using a class because a record will throw xml serializer warnings.
+    /// </summary>
+    public class ResultModel
+    {
+        public int Id { get; set; }
+        public string Text { get; set; } = null!;
+    }
 
     public class XmlRoot
     {
@@ -24,18 +31,18 @@ public class FluentHttpRequestController : ControllerBase
     public string QueryStringTest() => $"{Request.Query["Q1"]}:{Request.Query["Q2"]}";
 
     [HttpGet("SimpleJsonPayload")]
-    public ResultModel SimpleJsonPayload() => new(9999, "1111");
+    public ResultModel SimpleJsonPayload() => new() { Id = 9999, Text = "1111" };
 
     [HttpGet("SimpleXmlPayload")]
     public XmlRoot SimpleXmlPayload() => new() { Id = 5 };
 
     [HttpGet("SimpleJsonPayloadWithJsonParameters")]
-    public ResultModel SimpleJsonPayloadWithJsonParameters([FromBody] ResultModel parameters) => new(parameters.Id + 1, parameters.Text + "_Result");
+    public ResultModel SimpleJsonPayloadWithJsonParameters([FromBody] ResultModel parameters) => new() { Id = parameters.Id + 1, Text = parameters.Text + "_Result" };
 
     [Consumes("application/x-www-form-urlencoded")]
     [HttpGet("FormsEncodedParameters")]
-    public IEnumerable<ResultModel> FormsEncodedParameters([FromForm] IFormCollection parameters) => parameters.Select(t => new ResultModel(Convert.ToInt32(t.Key) + 10, t.Value + "_Result"));
+    public IEnumerable<ResultModel> FormsEncodedParameters([FromForm] IFormCollection parameters) => parameters.Select(t => new ResultModel() { Id = Convert.ToInt32(t.Key) + 10, Text = t.Value + "_Result" });
 
     [HttpPost("FileUploadStream")]
-    public IEnumerable<ResultModel> FileUploadStream(IEnumerable<IFormFile> formFiles) => formFiles.Select(file => new ResultModel(Convert.ToInt32(file.Length), file.FileName));
+    public IEnumerable<ResultModel> FileUploadStream(IEnumerable<IFormFile> formFiles) => formFiles.Select(file => new ResultModel() { Id = Convert.ToInt32(file.Length), Text = file.FileName });
 }
