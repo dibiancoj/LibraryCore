3e2c9ac17c044d4408b5fbf0b6978bb0b34e0baa
Jason Dibianco <dibiancj@mskcc.org>
2023-01-03 10:28:00 -0500

    Kakfa: Use the short hand to create a timespan

--- Src/LibraryCore.Kafka/IKafkaProcessor.cs
+++ Src/LibraryCore.Kafka/IKafkaProcessor.cs
@@ -6,7 +6,7 @@ public interface IKafkaProcessor<TKafkaKey, TKafkaMessageBody>
 {
     public IConsumer<TKafkaKey, TKafkaMessageBody> KafkaConsumer { get; }
     public IEnumerable<string> TopicsToRead { get; }
-    public TimeSpan KafkaConsumeTimeOut() => new(0, 0, 15);
+    public TimeSpan KafkaConsumeTimeOut() => TimeSpan.FromSeconds(15);
     public int NodeCount { get; }
     public Task ProcessMessageAsync(ConsumeResult<TKafkaKey, TKafkaMessageBody> messageResult, int nodeId, CancellationToken stoppingToken);
 }
