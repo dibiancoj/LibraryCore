3afcdf7058ecac5fff0afd400286ab1d1c8a8776
Jason DiBianco <dibiancoj@gmail.com>
2023-02-10 11:19:04 -0500

    Parser: Parameter debugging (#249)
    * Parser: Add parameter debugging as part of the debugging functionality
    
    * Add some code comments.
    
    ---------
    
    Co-authored-by: Jason Dibianco <dibiancj@mskcc.org>

--- Src/LibraryCore.Parsers/RuleParser/Logging/ExpressionLogger.cs
+++ Src/LibraryCore.Parsers/RuleParser/Logging/ExpressionLogger.cs
@@ -1,12 +1,24 @@
 ﻿namespace LibraryCore.Parsers.RuleParser.Logging;
 
 public record LogResult(string Message, bool Result);
+public record ParameterResult(string Name, string Value);
 
-public class ExpressionLogger
+public interface IExpressionLogger
+{
+    void AddLogRecord(string message, bool result);
+    void AddParameter(string parameterName, string parameterValue);
+    IEnumerable<LogResult> LogRecords();
+    IEnumerable<ParameterResult> ParameterRecords();
+}
+
+public class ExpressionLogger : IExpressionLogger
 {
     private List<LogResult> Records { get; } = new();
+    private Lazy<List<ParameterResult>> Parameters { get; } = new();
 
-    public void Add(LogResult logResult) => Records.Add(logResult);
+    public void AddLogRecord(string message, bool result) => Records.Add(new LogResult(message, result));
+    public void AddParameter(string parameterName, string parameterValue) => Parameters.Value.Add(new ParameterResult(parameterName, parameterValue));
 
     public IEnumerable<LogResult> LogRecords() => Records.AsEnumerable();
+    public IEnumerable<ParameterResult> ParameterRecords() => Parameters.Value;
 }
