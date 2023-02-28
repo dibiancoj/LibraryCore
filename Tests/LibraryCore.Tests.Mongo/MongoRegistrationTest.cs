8a51c9338f474023123ae702dfbe436d658f5f58
Jason Dibianco <dibiancj@mskcc.org>
2023-02-28 11:06:03 -0500

    document db add flag to ignore ssl cert issues when ssh'ing the database

--- Tests/LibraryCore.Tests.Mongo/MongoRegistrationTest.cs
+++ Tests/LibraryCore.Tests.Mongo/MongoRegistrationTest.cs
@@ -4,7 +4,7 @@ public class MongoRegistrationTest
 {
     [InlineData("sa", "password456", "mydocdbserver", "primary", false, "mongodb://sa:password456@mydocdbserver:27017/?ssl=true&retryWrites=False&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=primary")]
     [InlineData("root", "password123", "mydocdbserver", "secondary", false, "mongodb://root:password123@mydocdbserver:27017/?ssl=true&retryWrites=False&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondary")]
-    [InlineData("root", "password123", "mydocdbserver", "secondary", true, "mongodb://root:password123@mydocdbserver:27017/?ssl=true&retryWrites=False")]
+    [InlineData("root", "password123", "mydocdbserver", "secondary", true, "mongodb://root:password123@mydocdbserver:27017/?ssl=true&retryWrites=False&sslVerifyCertificate=false")]
     [Theory]
     public void ConnectionString(string userName, string password, string hostName, string readPreference, bool fromLocalHostWithSsh, string expectedConnectionString)
     {
@@ -14,7 +14,7 @@ public class MongoRegistrationTest
     [InlineData("sa", "password456", "mydocdbserver", false, 27017, "mongodb://sa:password456@mydocdbserver:27017/?ssl=true&retryWrites=False&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=primary")]
     [InlineData("root", "password123", "mydocdbserver", false, 27017, "mongodb://root:password123@mydocdbserver:27017/?ssl=true&retryWrites=False&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=primary")]
     [InlineData("root", "password123", "mydocdbserver", false, 9999, "mongodb://root:password123@mydocdbserver:9999/?ssl=true&retryWrites=False&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=primary")]
-    [InlineData("root", "password123", "mydocdbserver", true, 27017, "mongodb://root:password123@mydocdbserver:27017/?ssl=true&retryWrites=False")]
+    [InlineData("root", "password123", "mydocdbserver", true, 27017, "mongodb://root:password123@mydocdbserver:27017/?ssl=true&retryWrites=False&sslVerifyCertificate=false")]
     [Theory]
     public void ConnectionStringWithDefaultValue(string userName, string password, string hostName, bool fromLocalHostWithSsh, int hostPort, string expectedConnectionString)
     {
