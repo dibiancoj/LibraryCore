043c5b5fd1cb623427ebc73cadc9d1b7d550883b
Jason DiBianco <dibiancoj@gmail.com>
2023-05-17 21:53:18 -0400

    Days until bday (#268)

--- Tests/LibraryCore.Tests.Core/DateTimeUtilities/DateTimeUtilityTest.cs
+++ Tests/LibraryCore.Tests.Core/DateTimeUtilities/DateTimeUtilityTest.cs
@@ -1,4 +1,5 @@
 ﻿using LibraryCore.Core.DateTimeUtilities;
+using LibraryCore.Core.DateTimeUtilities.MockableDates;
 
 namespace LibraryCore.Tests.Core.DateTimeUtilities;
 
@@ -29,6 +30,40 @@ public class DateTimeUtilityTest
     public void CalculateAgeTest4()
     {
         Assert.Equal(2, DateTimeUtility.Age(DateTime.Today.AddYears(-2).AddDays(-3)));
+    }
+
+    #endregion
+
+    #region Get Days Until Bday
+
+    [Fact]
+    public void DaysUntilBdayTomorrow()
+    {
+        Assert.Equal(1, DateTimeUtility.DaysUntilNextBday(new FixedDateTimeProvider(DateTime.Today), DateTime.Today.AddDays(1)));
+    }
+
+    [Fact]
+    public void DaysUntilBdayToday()
+    {
+        Assert.Equal(0, DateTimeUtility.DaysUntilNextBday(new FixedDateTimeProvider(DateTime.Today), DateTime.Today));
+    }
+
+    [Fact]
+    public void DaysUntilBdayInThirtyDays()
+    {
+        Assert.Equal(0, DateTimeUtility.DaysUntilNextBday(new FixedDateTimeProvider(DateTime.Today), DateTime.Today.AddYears(30)));
+    }
+
+    [Fact]
+    public void DaysUntilBdayAlreadyPastInCurrentYear()
+    {
+        Assert.Equal(336, DateTimeUtility.DaysUntilNextBday(new FixedDateTimeProvider(new DateTime(2023, 5, 1)), new DateTime(2023, 4, 1)));
+    }
+
+    [Fact]
+    public void DaysUntilBdayDidntPassInCurrentYear()
+    {
+        Assert.Equal(92, DateTimeUtility.DaysUntilNextBday(new FixedDateTimeProvider(new DateTime(2023, 8, 1)), new DateTime(2023, 11, 1)));
     }
 
     #endregion
