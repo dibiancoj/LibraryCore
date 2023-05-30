93832281d7a5c2c203c8211862dc16e87519c361
Jason <dibiancoj@gmail.com>
2023-05-29 22:34:52 -0400

    aws secret manager

---
+++ Src/LibraryCore.AwsSecretManager/SecretManagerUtilities.cs
@@ -0,0 +1,58 @@
+﻿using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+using System.Text;
+using System.Text.Json;
+
+namespace LibraryCore.AwsSecretManager;
+
+public static class SecretManagerUtilities
+{
+    //AmazonSecretsManagerClient is the IAmazonSecretsManager implementation.
+    //In DI services.AddAWSService<IAmazonSecretsManager>();
+    //notes: Doesn't have support to get a specific version. Not needed so VersionId is not a parameter in the method
+    public static async Task<string?> GetSecretAsync(IAmazonSecretsManager client, string secretArnOrName, string versionStage = "AWSCURRENT", CancellationToken cancellationToken = default)
+    {
+        var tempResponse = await client.GetSecretValueAsync(new GetSecretValueRequest
+        {
+            SecretId = secretArnOrName,
+            VersionStage = versionStage
+        }, cancellationToken).ConfigureAwait(false);
+
+        if (tempResponse.HttpStatusCode != System.Net.HttpStatusCode.OK)
+        {
+            throw new Exception("Get Secret Value Async Resulted In " + tempResponse.HttpStatusCode);
+        }
+
+        if (tempResponse.SecretString is not null)
+        {
+            return tempResponse.SecretString;
+        }
+
+        if (tempResponse.SecretBinary is not null)
+        {
+            using var reader = new StreamReader(tempResponse.SecretBinary);
+            return Encoding.UTF8.GetString(Convert.FromBase64String(reader.ReadToEnd()));
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Json Object Version
+    /// </summary>
+    public static async Task<T?> GetSecretAsync<T>(IAmazonSecretsManager client,
+                                                        string secretArnOrName,
+                                                        string versionStage = "AWSCURRENT",
+                                                        JsonSerializerOptions? jsonSerializerOptions = null,
+                                                        CancellationToken cancellationToken = default)
+    {
+        var temp = await GetSecretAsync(client, secretArnOrName, versionStage, cancellationToken).ConfigureAwait(false);
+
+        if (string.IsNullOrEmpty(temp))
+        {
+            return default;
+        }
+
+        return JsonSerializer.Deserialize<T>(temp, jsonSerializerOptions);
+    }
+}
---
+++ Tests/LibraryCore.Tests.AwsSecretManager/SecretManagerUtilityTests.cs
@@ -0,0 +1,80 @@
+﻿using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+using LibraryCore.AwsSecretManager;
+using LibraryCore.Core.ExtensionMethods;
+
+namespace LibraryCore.Tests.AwsSecretManager;
+
+public class SecretManagerUtilityTests
+{
+    [Fact]
+    public async Task Non200Result()
+    {
+        var mockIAmazonSecretsManager = new Mock<IAmazonSecretsManager>();
+
+        mockIAmazonSecretsManager.Setup(x => x.GetSecretValueAsync(It.IsAny<GetSecretValueRequest>(), It.IsAny<CancellationToken>()))
+            .Returns(Task.FromResult(new GetSecretValueResponse
+            {
+                HttpStatusCode = System.Net.HttpStatusCode.NotFound
+            }));
+
+        var exception = await Assert.ThrowsAsync<Exception>(() =>
+        {
+            return SecretManagerUtilities.GetSecretAsync(mockIAmazonSecretsManager.Object, "MySecretName");
+        });
+
+        mockIAmazonSecretsManager.VerifyAll();
+    }
+
+    [Fact]
+    public async Task SuccessOnStringSecret()
+    {
+        var mockIAmazonSecretsManager = new Mock<IAmazonSecretsManager>();
+
+        mockIAmazonSecretsManager.Setup(x => x.GetSecretValueAsync(It.IsAny<GetSecretValueRequest>(), It.IsAny<CancellationToken>()))
+            .Returns(Task.FromResult(new GetSecretValueResponse
+            {
+                HttpStatusCode = System.Net.HttpStatusCode.OK,
+                SecretString = "test123"
+            }));
+
+        Assert.Equal("test123", await SecretManagerUtilities.GetSecretAsync(mockIAmazonSecretsManager.Object, "MySecretName"));
+
+        mockIAmazonSecretsManager.VerifyAll();
+    }
+
+    [Fact]
+    public async Task SuccessOnBinarySecret()
+    {
+        var mockIAmazonSecretsManager = new Mock<IAmazonSecretsManager>();
+
+        using var streamToMock = "test12345".ToBase64Encode().ToMemoryStream();
+
+        mockIAmazonSecretsManager.Setup(x => x.GetSecretValueAsync(It.IsAny<GetSecretValueRequest>(), It.IsAny<CancellationToken>()))
+            .Returns(Task.FromResult(new GetSecretValueResponse
+            {
+                HttpStatusCode = System.Net.HttpStatusCode.OK,
+                SecretBinary = streamToMock
+            }));
+
+        Assert.Equal("test12345", await SecretManagerUtilities.GetSecretAsync(mockIAmazonSecretsManager.Object, "MySecretName"));
+
+        mockIAmazonSecretsManager.VerifyAll();
+    }
+
+    [Fact]
+    public async Task BothStringAndBinaryAreNull()
+    {
+        var mockIAmazonSecretsManager = new Mock<IAmazonSecretsManager>();
+
+        mockIAmazonSecretsManager.Setup(x => x.GetSecretValueAsync(It.IsAny<GetSecretValueRequest>(), It.IsAny<CancellationToken>()))
+            .Returns(Task.FromResult(new GetSecretValueResponse
+            {
+                HttpStatusCode = System.Net.HttpStatusCode.OK
+            }));
+
+        Assert.Null(await SecretManagerUtilities.GetSecretAsync(mockIAmazonSecretsManager.Object, "MySecretName"));
+
+        mockIAmazonSecretsManager.VerifyAll();
+    }
+}
