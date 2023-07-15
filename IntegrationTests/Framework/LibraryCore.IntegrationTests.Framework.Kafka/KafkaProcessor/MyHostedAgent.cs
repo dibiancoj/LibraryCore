4732904557e6a93f3079107d537a17a124bb8a7d
Jason <dibiancoj@gmail.com>
2023-07-15 08:23:33 -0400

    cleanup of kafka. No runtime changes

---
+++ IntegrationTests/Framework/LibraryCore.IntegrationTests.Framework.Kafka/KafkaProcessor/MyHostedAgent.cs
@@ -0,0 +1,19 @@
+﻿using LibraryCore.Kafka;
+
+namespace LibraryCore.IntegrationTests.Framework.Kafka.KafkaProcessor;
+
+public class MyHostedAgent : BackgroundService
+{
+    public MyHostedAgent(KafkaNodeManager kafkaNodeManager)
+    {
+        KafkaNodeManager = kafkaNodeManager;
+    }
+
+    private KafkaNodeManager KafkaNodeManager { get; }
+    public const string KakfaJobName = "Job1";
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await Task.WhenAll(KafkaNodeManager.CreateNodeAsync(KakfaJobName, stoppingToken));
+    }
+}
