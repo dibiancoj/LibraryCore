5464fd611dec0a562c047c2c24ec88bd691ff765
Jason DiBianco <dibiancoj@gmail.com>
2023-10-07 16:54:29 -0400

    Aws secret manager (#291)
    * remove invalid comment
    
    * use pattern matching

--- Src/LibraryCore.AwsSecretManager/SecretManagerUtilities.cs
+++ Src/LibraryCore.AwsSecretManager/SecretManagerUtilities.cs
@@ -9,7 +9,6 @@ public static class SecretManagerUtilities
 {
     //AmazonSecretsManagerClient is the IAmazonSecretsManager implementation.
     //In DI services.AddAWSService<IAmazonSecretsManager>();
-    //notes: Doesn't have support to get a specific version. Not needed so VersionId is not a parameter in the method
     public static async Task<string?> GetSecretAsync(IAmazonSecretsManager client, string secretArnOrName, string versionStage = "AWSCURRENT", CancellationToken cancellationToken = default)
     {
         var tempResponse = await client.GetSecretValueAsync(new GetSecretValueRequest
@@ -23,18 +22,12 @@ public static class SecretManagerUtilities
             throw new Exception("Get Secret Value Async Resulted In " + tempResponse.HttpStatusCode);
         }
 
-        if (tempResponse.SecretString is not null)
+        return tempResponse switch
         {
-            return tempResponse.SecretString;
-        }
-
-        if (tempResponse.SecretBinary is not null)
-        {
-            using var reader = new StreamReader(tempResponse.SecretBinary);
-            return Encoding.UTF8.GetString(Convert.FromBase64String(reader.ReadToEnd()));
-        }
-
-        return null;
+            { SecretString: { } } => tempResponse.SecretString, //is not null pattern matching
+            { SecretBinary: { } } => StreamToString(tempResponse.SecretBinary), //is not null pattern matching
+            _ => null //fallback
+        };
     }
 
     /// <summary>
@@ -48,11 +41,14 @@ public static class SecretManagerUtilities
     {
         var temp = await GetSecretAsync(client, secretArnOrName, versionStage, cancellationToken).ConfigureAwait(false);
 
-        if (string.IsNullOrEmpty(temp))
-        {
-            return default;
-        }
+        return string.IsNullOrEmpty(temp) ?
+                    default :
+                    JsonSerializer.Deserialize<T>(temp, jsonSerializerOptions);
+    }
 
-        return JsonSerializer.Deserialize<T>(temp, jsonSerializerOptions);
+    private static string StreamToString(MemoryStream secretBinaryToConvert)
+    {
+        using var reader = new StreamReader(secretBinaryToConvert);
+        return Encoding.UTF8.GetString(Convert.FromBase64String(reader.ReadToEnd()));
     }
 }
