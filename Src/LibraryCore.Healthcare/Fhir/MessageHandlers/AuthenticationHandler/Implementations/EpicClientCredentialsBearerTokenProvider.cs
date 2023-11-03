dc728d2c55cc5419e5c87669de74652804f6812b
Jason <dibiancoj@gmail.com>
2023-11-02 20:26:07 -0400

    epic client credentials

---
+++ Src/LibraryCore.Healthcare/Fhir/MessageHandlers/AuthenticationHandler/Implementations/EpicClientCredentialsBearerTokenProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryCore.Healthcare.Fhir.MessageHandlers.AuthenticationHandler.Implementations;
+
+public class EpicClientCredentialsBearerTokenProvider : IFhirBearerTokenProvider
+{
+    public ValueTask<string> AccessTokenAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Tests/LibraryCore.Tests.Healthcare/Fhir/HttpClientHandlers/Implementations/EpicClientCredentialsBearerTokenProviderTest.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryCore.Tests.Healthcare.Fhir.HttpClientHandlers.Implementations;
+
+public class EpicClientCredentialsBearerTokenProviderTest
+{
+    [Fact]
+    public void CachingEpicClientCredsTest()
+    {
+        Assert.True(false);
+    }
+}
