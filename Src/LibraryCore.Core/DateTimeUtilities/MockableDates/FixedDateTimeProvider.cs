4848e74d5fb87e395164d06ca14d75faddbbf038
Jason <dibiancoj@gmail.com>
2023-11-14 19:40:53 -0500

    make date time provider obsolete

--- Src/LibraryCore.Core/DateTimeUtilities/MockableDates/FixedDateTimeProvider.cs
+++ Src/LibraryCore.Core/DateTimeUtilities/MockableDates/FixedDateTimeProvider.cs
@@ -3,6 +3,9 @@
 /// <summary>
 /// Provider where we can mock dates. This provider is great for unit tests
 /// </summary>
+#if NET8_0_OR_GREATER
+[Obsolete("Use TimeProvider In .net 8 Or C# 12")]
+#endif
 public class FixedDateTimeProvider : IDateTimeProvider
 {
     public FixedDateTimeProvider(DateTime fixedDate)
