49c7655a04cf916b0ec6bbe884bb3f4478d4d587
Jason DiBianco <dibiancoj@gmail.com>
2023-11-20 19:30:17 -0500

    seq guid creator (#306)

---
+++ Tests/LibraryCore.Tests.Core/ExtensionMethods/GuidExtensionMethodTest.cs
@@ -0,0 +1,33 @@
+﻿using LibraryCore.Core.ExtensionMethods;
+
+namespace LibraryCore.Tests.Core.ExtensionMethods;
+
+public static class GuidExtensionMethodTest
+{
+    [Fact]
+    public static async Task SequentialGuidCreatorTest()
+    {
+        var baseGuid = Guid.Empty;
+
+        await Task.Delay(TimeSpan.FromSeconds(1));
+
+        var secondGuid = baseGuid.SequentialGuidCreate();
+
+        await Task.Delay(TimeSpan.FromSeconds(1));
+
+        var thirdGuid = secondGuid.SequentialGuidCreate();
+
+        await Task.Delay(TimeSpan.FromSeconds(1));
+
+        var fourthGuid = thirdGuid.SequentialGuidCreate();
+
+        var allGuids = new[] { baseGuid, secondGuid, thirdGuid, fourthGuid }
+                        .OrderBy(x => x)
+                        .ToArray();
+
+        Assert.True(allGuids[0] == baseGuid);
+        Assert.True(allGuids[1] == secondGuid);
+        Assert.True(allGuids[2] == thirdGuid);
+        Assert.True(allGuids[3] == fourthGuid);
+    }
+}
