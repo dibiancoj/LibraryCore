b3cd7f12d9ef3c9ba0e88610795e383ae13b85a7
Jason DiBianco <dibiancoj@gmail.com>
2023-11-23 08:58:31 -0500

    shift left and shift right (#309)

---
+++ Src/LibraryCore.Core/ExtensionMethods/IListExtensionMethods.cs
@@ -0,0 +1,28 @@
+﻿namespace LibraryCore.Core.ExtensionMethods;
+
+public static class IListExtensionMethods
+{
+
+    /// <summary>
+    /// Shift elements to the right in a low level array operation
+    /// </summary>
+    public static void ShiftRight<T>(this IList<T> collection, int indexToStartShift)
+    {
+        for (int i = collection.Count - 1; i > indexToStartShift; i--)
+        {
+            collection[i] = collection[i - 1];
+        }
+    }
+
+    /// <summary>
+    /// Shift elements to the left in a low level array operation
+    /// </summary>
+    public static void ShiftLeft<T>(this IList<T> collection, int indexToStartShift)
+    {
+        for (var i = indexToStartShift; i < collection.Count - 1; i++)
+        {
+            collection[i] = collection[i + 1];
+        }
+    }
+
+}
