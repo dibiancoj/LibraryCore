c1a8686783083538604c6a52d21d31ec943e553b
Jason DiBianco <dibiancoj@gmail.com>
2024-01-04 15:31:12 -0500

    memory cache breaking change which will take a token instead of the source. (#329)

--- Tests/LibraryCore.Tests.Caching/MemoryCacheTest.cs
+++ Tests/LibraryCore.Tests.Caching/MemoryCacheTest.cs
@@ -76,37 +76,37 @@ public class MemoryCacheTest
     {
         int key1 = 1;
         int key2 = 2;
-        var cancelToken = new CancellationTokenSource();
+        var cancelTokenSource = new CancellationTokenSource();
 
         async Task<int> factory1(ICacheEntry x) => await Task.FromResult(key1);
         async Task<int> factory2(ICacheEntry x) => await Task.FromResult(key2);
 
         //pull from the cache
-        Assert.Equal(1, await InMemoryCacheServiceToUse.GetOrCreateWithLockAndEvictionAsync("key1", factory1, cancelToken));
-        Assert.Equal(2, await InMemoryCacheServiceToUse.GetOrCreateWithLockAndEvictionAsync("key2", factory2, cancelToken));
+        Assert.Equal(1, await InMemoryCacheServiceToUse.GetOrCreateWithLockAndEvictionAsync("key1", factory1, cancelTokenSource.Token));
+        Assert.Equal(2, await InMemoryCacheServiceToUse.GetOrCreateWithLockAndEvictionAsync("key2", factory2, cancelTokenSource.Token));
 
         key1 = 101;
         key2 = 102;
 
         //should get 1 since it's pulling from the cache
-        Assert.Equal(1, await InMemoryCacheServiceToUse.GetOrCreateWithLockAndEvictionAsync("key1", factory1, cancelToken));
-        Assert.Equal(2, await InMemoryCacheServiceToUse.GetOrCreateWithLockAndEvictionAsync("key2", factory2, cancelToken));
+        Assert.Equal(1, await InMemoryCacheServiceToUse.GetOrCreateWithLockAndEvictionAsync("key1", factory1, cancelTokenSource.Token));
+        Assert.Equal(2, await InMemoryCacheServiceToUse.GetOrCreateWithLockAndEvictionAsync("key2", factory2, cancelTokenSource.Token));
 
         //remove the cache
-        cancelToken.Cancel();
+        cancelTokenSource.Cancel();
 
         //create a new token
-        cancelToken = new CancellationTokenSource();
+        cancelTokenSource = new CancellationTokenSource();
 
         //both entries should be removed...so both should be the new field
-        Assert.Equal(101, await InMemoryCacheServiceToUse.GetOrCreateWithLockAndEvictionAsync("key1", factory1, cancelToken));
-        Assert.Equal(102, await InMemoryCacheServiceToUse.GetOrCreateWithLockAndEvictionAsync("key2", factory2, cancelToken));
+        Assert.Equal(101, await InMemoryCacheServiceToUse.GetOrCreateWithLockAndEvictionAsync("key1", factory1, cancelTokenSource.Token));
+        Assert.Equal(102, await InMemoryCacheServiceToUse.GetOrCreateWithLockAndEvictionAsync("key2", factory2, cancelTokenSource.Token));
 
         //clear it
-        cancelToken.Cancel();
+        cancelTokenSource.Cancel();
 
         //make sure it's not there now
-        Assert.Equal("FromSource", await InMemoryCacheServiceToUse.GetOrCreateWithLockAndEvictionAsync(999, x => Task.FromResult("FromSource"), cancelToken));
+        Assert.Equal("FromSource", await InMemoryCacheServiceToUse.GetOrCreateWithLockAndEvictionAsync(999, x => Task.FromResult("FromSource"), cancelTokenSource.Token));
     }
 
     #endregion
