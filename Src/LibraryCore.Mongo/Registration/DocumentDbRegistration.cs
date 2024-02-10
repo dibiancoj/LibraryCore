398206af1047485a2ebabc82701c332aacb941ac
Jason DiBianco <dibiancoj@gmail.com>
2024-02-09 20:05:41 -0500

    update mongo with cert (#337)

--- Src/LibraryCore.Mongo/Registration/DocumentDbRegistration.cs
+++ Src/LibraryCore.Mongo/Registration/DocumentDbRegistration.cs
@@ -10,6 +10,12 @@ namespace LibraryCore.Mongo.Registration;
 /// </summary>
 public static class DocumentDbRegistration
 {
+    public enum DocumentDbCertIdentifier
+    {
+        rds_ca_2019,
+        rds_ca_rsa2048_g1
+    }
+
     //to ssh with port forwarding
     //ssh -i "my-dev-ssh-key.pem" -L 27017:mycluster.us-east-1.docdb.amazonaws.com:27017 ec2-user@10.1.1.1 -N
 
@@ -17,7 +23,7 @@ public static class DocumentDbRegistration
     /// For encrypted at rest databases in lambdas
     /// </summary>
     [ExcludeFromCodeCoverage]
-    public static IMongoClient RegisterInLambdaEnvironment(string mongoConnectionString, bool runningFromLocalHostWithSsh = false)
+    public static IMongoClient RegisterInLambdaEnvironment(string mongoConnectionString, DocumentDbCertIdentifier documentDbCertIdentifier, bool runningFromLocalHostWithSsh = false)
     {
         var settings = MongoClientSettings.FromUrl(new MongoUrl(mongoConnectionString));
 
@@ -26,7 +32,7 @@ public static class DocumentDbRegistration
             ClientCertificates = new X509Certificate[]
             {
                  //use the pem file here
-                 new X509Certificate(Properties.Resources.rds_combined_ca_bundlepem)
+                 new(CaCertIdentifierFromResource(documentDbCertIdentifier, true))
             }
         };
 
@@ -39,11 +45,14 @@ public static class DocumentDbRegistration
     /// For encrypted at rest databases in ec2, containers, or anything where you can install the certificate
     /// </summary>
     [ExcludeFromCodeCoverage]
-    public static IMongoClient RegisterInWritableEnvironment(string connectionString, StoreName storeToLoad = StoreName.Root, bool runningFromLocalHostWithSsh = false)
+    public static IMongoClient RegisterInWritableEnvironment(string connectionString,
+                                                             DocumentDbCertIdentifier documentDbCertIdentifier,
+                                                             StoreName storeToLoad = StoreName.Root,
+                                                             bool runningFromLocalHostWithSsh = false)
     {
         using var localTrustStore = new X509Store(storeToLoad); //isLocalDev ? new X509Store(StoreName.My) : new X509Store(StoreName.Root);
         var certificateCollection = new X509Certificate2Collection();
-        certificateCollection.Import(Properties.Resources.rds_combined_ca_bundlep7b);
+        certificateCollection.Import(CaCertIdentifierFromResource(documentDbCertIdentifier, false));
 
         localTrustStore.Open(OpenFlags.ReadWrite);
         localTrustStore.AddRange(certificateCollection);
@@ -56,15 +65,43 @@ public static class DocumentDbRegistration
     }
 
     //retry writes aren't supported in document db. Always have that false unless your using the real mongo
-    public static string EncryptedMongoConnectionStringBuilder(string userName, string password, string dbHostName, string readPreference = "primary", bool runningFromLocalHostWithSsh = false, int port = 27017, bool retryWrites = false)
+    public static string EncryptedMongoConnectionStringBuilder(string userName,
+                                                               string password,
+                                                               string dbHostName,
+                                                               DocumentDbCertIdentifier documentDbCertIdentifier,
+                                                               string readPreference = "primary",
+                                                               bool runningFromLocalHostWithSsh = false,
+                                                               int port = 27017,
+                                                               bool retryWrites = false)
     {
         var builder = new StringBuilder($"mongodb://{userName}:{password}@{dbHostName}:{port}/?ssl=true&retryWrites={retryWrites}");
 
-        builder.Append(runningFromLocalHostWithSsh ?
-                            "&sslVerifyCertificate=false" :
-                            $"&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference={readPreference}");
+        if (runningFromLocalHostWithSsh)
+        {
+            builder.Append("&sslVerifyCertificate=false");
+        }
+        else
+        {
+            var identifierToFileName = documentDbCertIdentifier == DocumentDbCertIdentifier.rds_ca_2019 ?
+                                                             "rds-combined-ca-bundle.pem" :
+                                                             "global-bundle.pem";
+
+            builder.Append($"&ssl_ca_certs={identifierToFileName}&replicaSet=rs0&readPreference={readPreference}");
+        }
 
         return builder.ToString();
     }
+
+    private static byte[] CaCertIdentifierFromResource(DocumentDbCertIdentifier documentDbCertIdentifier, bool pemVersion) =>
+        (documentDbCertIdentifier, pemVersion) switch
+        {
+            (DocumentDbCertIdentifier.rds_ca_2019, false) => Properties.Resources.rds_combined_ca_bundlep7b,
+            (DocumentDbCertIdentifier.rds_ca_2019, true) => Properties.Resources.rds_combined_ca_bundlepem,
+
+            (DocumentDbCertIdentifier.rds_ca_rsa2048_g1, false) => Properties.Resources.global_bundlep7b,
+            (DocumentDbCertIdentifier.rds_ca_rsa2048_g1, true) => Properties.Resources.global_bundlepem,
+
+            _ => throw new NotImplementedException()
+        };
 
 }
--- Tests/LibraryCore.Tests.Mongo/MongoRegistrationTest.cs
+++ Tests/LibraryCore.Tests.Mongo/MongoRegistrationTest.cs
@@ -1,24 +1,35 @@
-﻿namespace LibraryCore.Tests.Mongo;
+﻿using LibraryCore.Mongo.Registration;
+using static LibraryCore.Mongo.Registration.DocumentDbRegistration;
+
+namespace LibraryCore.Tests.Mongo;
 
 public class MongoRegistrationTest
 {
-    [InlineData("sa", "password456", "mydocdbserver", "primary", false, "mongodb://sa:password456@mydocdbserver:27017/?ssl=true&retryWrites=False&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=primary")]
-    [InlineData("root", "password123", "mydocdbserver", "secondary", false, "mongodb://root:password123@mydocdbserver:27017/?ssl=true&retryWrites=False&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondary")]
-    [InlineData("root", "password123", "mydocdbserver", "secondary", true, "mongodb://root:password123@mydocdbserver:27017/?ssl=true&retryWrites=False&sslVerifyCertificate=false")]
+    [InlineData("sa", "password456", "mydocdbserver", "primary", false, DocumentDbCertIdentifier.rds_ca_2019, "mongodb://sa:password456@mydocdbserver:27017/?ssl=true&retryWrites=False&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=primary")]
+    [InlineData("root", "password123", "mydocdbserver", "secondary", false, DocumentDbCertIdentifier.rds_ca_2019, "mongodb://root:password123@mydocdbserver:27017/?ssl=true&retryWrites=False&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondary")]
+    [InlineData("root", "password123", "mydocdbserver", "secondary", true, DocumentDbCertIdentifier.rds_ca_2019, "mongodb://root:password123@mydocdbserver:27017/?ssl=true&retryWrites=False&sslVerifyCertificate=false")]
+    [InlineData("sa", "password456", "mydocdbserver", "primary", false, DocumentDbCertIdentifier.rds_ca_rsa2048_g1, "mongodb://sa:password456@mydocdbserver:27017/?ssl=true&retryWrites=False&ssl_ca_certs=global-bundle.pem&replicaSet=rs0&readPreference=primary")]
+    [InlineData("root", "password123", "mydocdbserver", "secondary", false, DocumentDbCertIdentifier.rds_ca_rsa2048_g1, "mongodb://root:password123@mydocdbserver:27017/?ssl=true&retryWrites=False&ssl_ca_certs=global-bundle.pem&replicaSet=rs0&readPreference=secondary")]
+    [InlineData("root", "password123", "mydocdbserver", "secondary", true, DocumentDbCertIdentifier.rds_ca_rsa2048_g1, "mongodb://root:password123@mydocdbserver:27017/?ssl=true&retryWrites=False&sslVerifyCertificate=false")]
+
     [Theory]
-    public void ConnectionString(string userName, string password, string hostName, string readPreference, bool fromLocalHostWithSsh, string expectedConnectionString)
+    public void ConnectionString(string userName, string password, string hostName, string readPreference, bool fromLocalHostWithSsh, DocumentDbCertIdentifier documentDbRegistration, string expectedConnectionString)
     {
-        Assert.Equal(expectedConnectionString, LibraryCore.Mongo.Registration.DocumentDbRegistration.EncryptedMongoConnectionStringBuilder(userName, password, hostName, readPreference: readPreference, runningFromLocalHostWithSsh: fromLocalHostWithSsh));
+        Assert.Equal(expectedConnectionString, EncryptedMongoConnectionStringBuilder(userName, password, hostName, documentDbRegistration, readPreference: readPreference, runningFromLocalHostWithSsh: fromLocalHostWithSsh));
     }
 
-    [InlineData("sa", "password456", "mydocdbserver", false, 27017, "mongodb://sa:password456@mydocdbserver:27017/?ssl=true&retryWrites=False&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=primary")]
-    [InlineData("root", "password123", "mydocdbserver", false, 27017, "mongodb://root:password123@mydocdbserver:27017/?ssl=true&retryWrites=False&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=primary")]
-    [InlineData("root", "password123", "mydocdbserver", false, 9999, "mongodb://root:password123@mydocdbserver:9999/?ssl=true&retryWrites=False&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=primary")]
-    [InlineData("root", "password123", "mydocdbserver", true, 27017, "mongodb://root:password123@mydocdbserver:27017/?ssl=true&retryWrites=False&sslVerifyCertificate=false")]
+    [InlineData("sa", "password456", "mydocdbserver", false, 27017, DocumentDbCertIdentifier.rds_ca_2019, "mongodb://sa:password456@mydocdbserver:27017/?ssl=true&retryWrites=False&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=primary")]
+    [InlineData("root", "password123", "mydocdbserver", false, 27017, DocumentDbCertIdentifier.rds_ca_2019, "mongodb://root:password123@mydocdbserver:27017/?ssl=true&retryWrites=False&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=primary")]
+    [InlineData("root", "password123", "mydocdbserver", false, 9999, DocumentDbCertIdentifier.rds_ca_2019, "mongodb://root:password123@mydocdbserver:9999/?ssl=true&retryWrites=False&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=primary")]
+    [InlineData("root", "password123", "mydocdbserver", true, 27017, DocumentDbCertIdentifier.rds_ca_2019, "mongodb://root:password123@mydocdbserver:27017/?ssl=true&retryWrites=False&sslVerifyCertificate=false")]
+    [InlineData("sa", "password456", "mydocdbserver", false, 27017, DocumentDbCertIdentifier.rds_ca_rsa2048_g1, "mongodb://sa:password456@mydocdbserver:27017/?ssl=true&retryWrites=False&ssl_ca_certs=global-bundle.pem&replicaSet=rs0&readPreference=primary")]
+    [InlineData("root", "password123", "mydocdbserver", false, 27017, DocumentDbCertIdentifier.rds_ca_rsa2048_g1, "mongodb://root:password123@mydocdbserver:27017/?ssl=true&retryWrites=False&ssl_ca_certs=global-bundle.pem&replicaSet=rs0&readPreference=primary")]
+    [InlineData("root", "password123", "mydocdbserver", false, 9999, DocumentDbCertIdentifier.rds_ca_rsa2048_g1, "mongodb://root:password123@mydocdbserver:9999/?ssl=true&retryWrites=False&ssl_ca_certs=global-bundle.pem&replicaSet=rs0&readPreference=primary")]
+    [InlineData("root", "password123", "mydocdbserver", true, 27017, DocumentDbCertIdentifier.rds_ca_rsa2048_g1, "mongodb://root:password123@mydocdbserver:27017/?ssl=true&retryWrites=False&sslVerifyCertificate=false")]
     [Theory]
-    public void ConnectionStringWithDefaultValue(string userName, string password, string hostName, bool fromLocalHostWithSsh, int hostPort, string expectedConnectionString)
+    public void ConnectionStringWithDefaultValue(string userName, string password, string hostName, bool fromLocalHostWithSsh, int hostPort, DocumentDbCertIdentifier documentDbRegistration, string expectedConnectionString)
     {
-        Assert.Equal(expectedConnectionString, LibraryCore.Mongo.Registration.DocumentDbRegistration.EncryptedMongoConnectionStringBuilder(userName, password, hostName, port: hostPort, runningFromLocalHostWithSsh: fromLocalHostWithSsh));
+        Assert.Equal(expectedConnectionString, EncryptedMongoConnectionStringBuilder(userName, password, hostName, documentDbRegistration, port: hostPort, runningFromLocalHostWithSsh: fromLocalHostWithSsh));
     }
 
 }
