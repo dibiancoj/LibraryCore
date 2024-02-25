9a2b187c730fb428381a8f92fa3718a49eb0ee32
Jason <dibiancoj@gmail.com>
2024-02-25 11:44:07 -0500

    update

--- Src/LibraryCore.Shared/LibraryCore.Aot.Json/ResolveJsonType.cs
+++ Src/LibraryCore.Shared/LibraryCore.Aot.Json/ResolveJsonType.cs
@@ -7,17 +7,10 @@ namespace LibraryCore.Aot.Json;
 
 public static class ResolveJsonType
 {
-    static ResolveJsonType()
-    {
-        WebDefaultSerializerOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web)
-        {
-            TypeInfoResolver = new DefaultJsonTypeInfoResolver()
-        };
-    }
     //most of this is taken from System.Net.Http.Json.JsonHelpers to allow non aot to use the same code and pull back json type info using web options.
     //this way camel case works by default
     //https://github.com/dotnet/runtime/blob/main/src/libraries/System.Net.Http.Json/src/System/Net/Http/Json/JsonHelpers.cs
-    private static JsonSerializerOptions WebDefaultSerializerOptions { get; }
+    private static JsonSerializerOptions? WebDefaultSerializerOptions { get; set; }
 
     [RequiresUnreferencedCode(ErrorMessages.AotDynamicAccess)]
 #if NET7_0_OR_GREATER
@@ -26,7 +19,12 @@ public static class ResolveJsonType
     public static JsonTypeInfo<T> ResolveJsonTypeInfo<T>(JsonSerializerDefaults serializationDefault = JsonSerializerDefaults.Web)
     {
         return serializationDefault == JsonSerializerDefaults.Web ?
-            (JsonTypeInfo<T>)WebDefaultSerializerOptions.GetTypeInfo(typeof(T)) :
+            (JsonTypeInfo<T>)(WebDefaultSerializerOptions ??= CreateWebSerializer()).GetTypeInfo(typeof(T)) :
             (JsonTypeInfo<T>)JsonSerializerOptions.Default.GetTypeInfo(typeof(T));
     }
+
+    private static JsonSerializerOptions CreateWebSerializer() => new(JsonSerializerDefaults.Web)
+    {
+        TypeInfoResolver = new DefaultJsonTypeInfoResolver()
+    };
 }
