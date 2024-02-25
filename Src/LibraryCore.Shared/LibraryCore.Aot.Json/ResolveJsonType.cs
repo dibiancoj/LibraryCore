528224cd08562b63f0edce01b2bb4b4c85b7ce38
Jason DiBianco <dibiancoj@gmail.com>
2024-02-25 14:15:02 -0500

    Aot move to creator (#353)
    * update
    
    * update

--- Src/LibraryCore.Shared/LibraryCore.Aot.Json/ResolveJsonType.cs
+++ Src/LibraryCore.Shared/LibraryCore.Aot.Json/ResolveJsonType.cs
@@ -7,17 +7,10 @@ namespace LibraryCore.Aot.Json;
 
 public static class ResolveJsonType
 {
-    static ResolveJsonType()
-    {
-        WebDefaultSerializerOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web)
-        {
-            TypeInfoResolver = new DefaultJsonTypeInfoResolver()
-        };
-    }
     //most of this is taken from System.Net.Http.Json.JsonHelpers to allow non aot to use the same code and pull back json type info using web options.
     //this way camel case works by default
     //https://github.com/dotnet/runtime/blob/main/src/libraries/System.Net.Http.Json/src/System/Net/Http/Json/JsonHelpers.cs
-    private static JsonSerializerOptions WebDefaultSerializerOptions { get; }
+    private static JsonSerializerOptions? WebDefaultSerializerOptions { get; set; }
 
     [RequiresUnreferencedCode(ErrorMessages.AotDynamicAccess)]
 #if NET7_0_OR_GREATER
@@ -25,8 +18,28 @@ public static class ResolveJsonType
 #endif
     public static JsonTypeInfo<T> ResolveJsonTypeInfo<T>(JsonSerializerDefaults serializationDefault = JsonSerializerDefaults.Web)
     {
-        return serializationDefault == JsonSerializerDefaults.Web ?
-            (JsonTypeInfo<T>)WebDefaultSerializerOptions.GetTypeInfo(typeof(T)) :
-            (JsonTypeInfo<T>)JsonSerializerOptions.Default.GetTypeInfo(typeof(T));
+        return (JsonTypeInfo<T>)ResolveOptionsToUse(null, serializationDefault).GetTypeInfo(typeof(T));
     }
+
+    public static JsonTypeInfo<T> ResolveJsonTypeInfo<T>(JsonSerializerOptions? jsonSerializerOptions)
+    {
+        return (JsonTypeInfo<T>)ResolveOptionsToUse(jsonSerializerOptions, JsonSerializerDefaults.Web).GetTypeInfo(typeof(T));
+    }
+
+    private static JsonSerializerOptions ResolveOptionsToUse(JsonSerializerOptions? jsonSerializerOptions, JsonSerializerDefaults serializationDefault)
+    {
+        if (jsonSerializerOptions is not null)
+        {
+            return jsonSerializerOptions;
+        }
+
+        return serializationDefault == JsonSerializerDefaults.Web ?
+                                            (WebDefaultSerializerOptions ??= CreateWebSerializer()) :
+                                            JsonSerializerOptions.Default;
+    }
+
+    private static JsonSerializerOptions CreateWebSerializer() => new(JsonSerializerDefaults.Web)
+    {
+        TypeInfoResolver = new DefaultJsonTypeInfoResolver()
+    };
 }
