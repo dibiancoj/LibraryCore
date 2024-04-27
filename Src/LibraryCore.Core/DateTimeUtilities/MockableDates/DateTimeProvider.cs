f7e35c41e6618ac17937613fb5956eb4455ae740
Jason DiBianco <dibiancoj@gmail.com>
2024-04-26 22:31:54 -0400

    use time provider polyfill for < .net 8 (#354)
    * use time provider polyfill for < .net 8
    
    * update
    
    * update to be utc.

--- Src/LibraryCore.Core/DateTimeUtilities/MockableDates/DateTimeProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LibraryCore.Core.DateTimeUtilities.MockableDates;
-
-#if NET8_0_OR_GREATER
-[Obsolete("Use TimeProvider In .net 8 Or C# 12")]
-#endif
-public class DateTimeProvider : IDateTimeProvider
-{
-    public DateTime GetNow() => DateTime.Now;
-}
