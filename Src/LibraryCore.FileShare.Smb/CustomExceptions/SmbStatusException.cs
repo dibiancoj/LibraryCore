4414f32231031025711fccbcb702096d7aa6cb39
Dibianco, Jason <dibiancj@mskcc.org>
2024-09-20 08:46:12 -0400

    Add smb file share.

---
+++ Src/LibraryCore.FileShare.Smb/CustomExceptions/SmbStatusException.cs
@@ -0,0 +1,14 @@
+﻿using SMBLibrary;
+
+namespace LibraryCore.FileShare.Smb.CustomExceptions;
+
+public class SmbStatusException(NTStatus statusFound, string? Expression = null) : Exception
+{
+    public NTStatus StatusFound { get; } = statusFound;
+    public string? Expression { get; } = Expression;
+
+    public override string ToString()
+    {
+        return $"SMB Status Exception: StatusId = {StatusFound} - Expression = {Expression}";
+    }
+}
---
+++ Tests/LibraryCore.Tests.FileShare.Smb/ThrowUtilitiesTest.cs
@@ -0,0 +1,28 @@
+﻿using LibraryCore.FileShare.Smb.CustomExceptions;
+using LibraryCore.FileShare.Smb.ExtensionMethods;
+using SMBLibrary;
+
+namespace LibraryCore.Tests.FileShare.Smb;
+
+public class ThrowUtilitiesTest
+{
+    [Fact]
+    public void ThrowIfNotSuccessful_SuccessfulStatus() => NTStatus.STATUS_SUCCESS.ThrowIfNotSuccessful();
+
+    [Fact]
+    public void ThrowIfNotSuccessful_NotSuccessfulStatus() => Assert.Throws<SmbStatusException>(() => NTStatus.STATUS_END_OF_FILE.ThrowIfNotSuccessful());
+
+    [InlineData(NTStatus.STATUS_END_OF_FILE)]
+    [InlineData(NTStatus.STATUS_SUCCESS)]
+    [Theory]
+    public void ThrowIfNotStatus_SuccessfulStatus(NTStatus status) => status.ThrowIfNotStatus([NTStatus.STATUS_END_OF_FILE, .. ThrowUtilities.OnlySuccess]);
+
+    [Fact]
+    public void ThrowIfNotStatus_NotSuccessfulStatus()
+    {
+        foreach (var enumValue in Enum.GetValues<NTStatus>().Where(x => x != NTStatus.STATUS_SUCCESS && x != NTStatus.STATUS_END_OF_FILE))
+        {
+            Assert.Throws<SmbStatusException>(() => enumValue.ThrowIfNotStatus([NTStatus.STATUS_END_OF_FILE, .. ThrowUtilities.OnlySuccess]));
+        }
+    }
+}
