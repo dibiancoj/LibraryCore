f780e454d08388b234205a9a6d88aa66205f024f
Jason DiBianco <dibiancoj@gmail.com>
2024-10-20 19:40:32 -0400

    IsBetween

---
+++ Tests/LibraryCore.Tests.Core/ExtensionMethods/INumberExtensionMethodTest.cs
@@ -0,0 +1,43 @@
+﻿using LibraryCore.Core.ExtensionMethods;
+
+namespace LibraryCore.Tests.Core.ExtensionMethods;
+
+public class INumberExtensionMethodTest
+{
+#if NET7_0_OR_GREATER
+
+    [InlineData(3, 1, 5, true)] //basic true test
+    [InlineData(0, 1, 5, false)] //base false test (number lower then lower band)
+    [InlineData(6, 1, 5, false)] //base false test (number higher then higher band)
+    [InlineData(1, 1, 5, true)] //same number on lower band should be true
+    [InlineData(5, 1, 5, false)] //same number on upper band should be false
+    [Theory]
+    public void IsNumberBetween_Int(int value, int lower, int upper, bool expected)
+    {
+        Assert.Equal(expected, value.IsBetween(lower, upper));
+    }
+
+    [InlineData(3, 1, 5, true)] //basic true test
+    [InlineData(0, 1, 5, false)] //base false test (number lower then lower band)
+    [InlineData(6, 1, 5, false)] //base false test (number higher then higher band)
+    [InlineData(1, 1, 5, true)] //same number on lower band should be true
+    [InlineData(5, 1, 5, false)] //same number on upper band should be false
+    [Theory]
+    public void IsNumberBetween_Double(double value, double lower, double upper, bool expected)
+    {
+        Assert.Equal(expected, value.IsBetween(lower, upper));
+    }
+
+    [InlineData(3, 1, 5, true)] //basic true test
+    [InlineData(0, 1, 5, false)] //base false test (number lower then lower band)
+    [InlineData(6, 1, 5, false)] //base false test (number higher then higher band)
+    [InlineData(1, 1, 5, true)] //same number on lower band should be true
+    [InlineData(5, 1, 5, false)] //same number on upper band should be false
+    [Theory]
+    public void IsNumberBetween_Float(float value, float lower, float upper, bool expected)
+    {
+        Assert.Equal(expected, value.IsBetween(lower, upper));
+    }
+
+#endif
+}
